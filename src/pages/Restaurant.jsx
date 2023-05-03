3059a2a2f7dc878ccf2f9968f4595fbccabc8a56
brndalx <brndalx@gmail.com>
2023-05-03 15:37:47 +0300

    feat: basic restaurant layout page and fonts

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -1,5 +1,35 @@
+import { Box, Container, Flex, GridItem, Text, Image, Grid } from '@chakra-ui/react';
 import React from 'react';
 
 export default function Restaurant() {
-  return <div>Restaurant</div>;
+  return (
+    <>
+      <Container maxW='1110px'>
+        <Box>
+          <Box>
+            <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: '1.3fr 1fr' }} gap={2}>
+              <GridItem w='100%' h='auto' bg='primary.light' borderRadius={20}>
+                <Flex alignItems='center'>
+                  <Box w='50%'>
+                    <Text fontSize='sm' color='neutral.black' fontWeight='medium'>
+                      here text
+                    </Text>
+                  </Box>
+                </Flex>
+              </GridItem>
+              <GridItem w='100%' h='auto' bg='primary.light' borderRadius={20}>
+                <Flex alignItems='center'>
+                  <Box w='50%'>
+                    <Text fontSize='sm' color='neutral.black' fontWeight='medium'>
+                      here text
+                    </Text>
+                  </Box>
+                </Flex>
+              </GridItem>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
 }
