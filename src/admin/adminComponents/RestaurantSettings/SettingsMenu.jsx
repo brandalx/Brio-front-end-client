b3d4f6f81141870512a68be7d403fc0cfe59fe9f
brndalx <brndalx@gmail.com>
2023-05-08 17:29:17 +0300

    feat: settings menu part

--- src/admin/adminComponents/RestaurantSettings/SettingsMenu.jsx
+++ src/admin/adminComponents/RestaurantSettings/SettingsMenu.jsx
@@ -1,5 +1,52 @@
+import { Box, Divider, Flex, GridItem, Heading, Text, Image, MenuButton } from '@chakra-ui/react';
 import React from 'react';
-
+import Account from '../../../assets/svg/Account';
+import Administrators from '../../../assets/svg/Administrators';
+import Security from '../../../assets/svg/Security';
+import theme from '../../../utils/theme';
 export default function SettingsMenu() {
-  return <div>SettingsMenu</div>;
+  const arr = [
+    { id: 1, title: 'Account', description: 'Restaurant information', icon: Account },
+    { id: 2, title: 'Administrators', description: 'Invite and manage admins', icon: Administrators },
+    {
+      id: 3,
+      title: 'Security',
+      description: 'Password, 2FA',
+      icon: Security
+    }
+  ];
+  return (
+    <Box>
+      <Text mb='16px' fontSize='s' fontWeight='semibold' color='neutral.black'>
+        Settings
+      </Text>
+      {arr.map((element) => {
+        const Icon = element.icon;
+        return (
+          <Box
+            key={element.id}
+            borderRadius='16px'
+            mb='12px'
+            p='10px'
+            borderWidth='1px'
+            borderColor='neutral.grayLightest'
+          >
+            <Flex alignItems='center'>
+              <Box me={4} p={3} bg='neutral.grayLightest' borderRadius={12}>
+                <Icon color={theme.colors.primary.default} />
+              </Box>
+              <Box>
+                <Heading fontSize='2xs' fontWeight='bold' color='neutral.black'>
+                  {element.title}
+                </Heading>
+                <Text fontSize='3xs' color='neutral.grayDark'>
+                  {element.description}
+                </Text>
+              </Box>
+            </Flex>
+          </Box>
+        );
+      })}
+    </Box>
+  );
 }
--- src/admin/adminPages/RestaurantSettings.jsx
+++ src/admin/adminPages/RestaurantSettings.jsx
@@ -1,15 +1,21 @@
-import { Container, Grid, GridItem } from '@chakra-ui/react';
+import { Box, Container, Grid, GridItem } from '@chakra-ui/react';
 import React from 'react';
+import SettingsMenu from '../adminComponents/RestaurantSettings/SettingsMenu';
 
 export default function RestaurantSettings() {
   return (
     <>
-      <Container maxW='1110px'>
-        <Grid templateColumns={{ sm: 'repeat(4, 1fr)', md: 'repeat(8, 1fr)' }} gap={6}>
-          <GridItem colSpan={{ sm: 4, md: 2 }}>part1</GridItem>
-          <GridItem colSpan={{ sm: 4, md: 6 }}>part two2</GridItem>
-        </Grid>
-      </Container>
+      <Box py={5}>
+        <Container maxW='1110px'>
+          <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: '1fr 2fr ' }} gap={6}>
+            <GridItem w='100%'>
+              {' '}
+              <SettingsMenu />{' '}
+            </GridItem>
+            <GridItem w='100%'>part two2</GridItem>
+          </Grid>
+        </Container>
+      </Box>
     </>
   );
 }
