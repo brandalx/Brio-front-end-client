c9004cdb47ace217151ee1dfa653193fab423768
brndalx <brndalx@gmail.com>
2023-05-09 19:40:14 +0300

    feat: administrators and security pages basic

--- src/admin/adminComponents/RestaurantSettings/Administrators.jsx
+++ src/admin/adminComponents/RestaurantSettings/Administrators.jsx
@@ -1,5 +1,34 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Text,
+  Image,
+  FormControl,
+  FormLabel,
+  Input,
+  GridItem,
+  Grid,
+  Textarea,
+  Divider,
+  Checkbox,
+  Stack
+} from '@chakra-ui/react';
 import React from 'react';
 
 export default function Administrators() {
-  return <div>Administrators</div>;
+  return (
+    <Box>
+      <Text mb='16px' fontSize='sm' fontWeight='semibold' color='neutral.black'>
+        Administrators
+      </Text>
+      <Box borderRadius='16px' borderWidth='1px' py='20px' px='10px'>
+        <Text fontSize='xs' fontWeight='bold' color='neutral.black'>
+          Invite and manage admins
+        </Text>
+        <Box pt={5}>box1</Box>
+        <Box pt={5}>box2</Box>
+      </Box>
+    </Box>
+  );
 }
--- src/admin/adminComponents/RestaurantSettings/Security.jsx
+++ src/admin/adminComponents/RestaurantSettings/Security.jsx
@@ -1,5 +1,36 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Text,
+  Image,
+  FormControl,
+  FormLabel,
+  Input,
+  GridItem,
+  Grid,
+  Textarea,
+  Divider,
+  Checkbox,
+  Stack
+} from '@chakra-ui/react';
 import React from 'react';
 
 export default function Security() {
-  return <div>Security</div>;
+  return (
+    <>
+      <Box>
+        <Text mb='16px' fontSize='sm' fontWeight='semibold' color='neutral.black'>
+          Security
+        </Text>
+        <Box borderRadius='16px' borderWidth='1px' py='20px' px='10px'>
+          <Text fontSize='xs' fontWeight='bold' color='neutral.black'>
+            2 factor authentication
+          </Text>
+          <Box pt={5}>box1</Box>
+          <Box pt={5}>box2</Box>
+        </Box>
+      </Box>
+    </>
+  );
 }
