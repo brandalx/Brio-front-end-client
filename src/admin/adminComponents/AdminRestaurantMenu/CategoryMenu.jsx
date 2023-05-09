9473ec52fff6d8eb654b42cecbaed56723422ffc
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-05-10 01:58:30 +0300

    feat: adaptive RestaurantMenu.jsx made

--- src/admin/adminComponents/AdminRestaurantMenu/CategoryMenu.jsx
+++ src/admin/adminComponents/AdminRestaurantMenu/CategoryMenu.jsx
@@ -1,31 +1,45 @@
 import React from 'react';
-import {Box, Button, Divider, GridItem, Heading, Text} from "@chakra-ui/react";
+import {Box, Button, Divider, GridItem, Heading, Text, useMediaQuery} from "@chakra-ui/react";
 import AddPlus from "../../../assets/svg/AddPlus";
-import theme from '../../../utils/theme'
+import theme from '../../../utils/theme';
 
 export default function CategoryMenu() {
     const arr = [{id: 1, title: 'Breakfast menu', amount: 17}, {id: 2, title: 'Lunch menu', amount: 27}, {
-        id: 3, title: 'Dinner menu',
+        id: 3,
+        title: 'Dinner menu',
         amount: 12
-    }, {title: 'Drinks menu', amount: 15}]
+    }, {title: 'Drinks menu', amount: 15}];
+
+    const [isTablet] = useMediaQuery("(max-width: 992px)");
+    const [isMobile] = useMediaQuery("(max-width: 576px)");
+
     return (
         <GridItem colSpan={4}>
             <Text mb='16px' fontSize='sm' fontWeight={theme.fontWeights.semibold} color='neutral.black'>Category
                 menu</Text>
-            {arr.map(element => {
-                return (<Box key={element.id} borderRadius='16px' mb='12px' p='10px' border='1px solid #EDEEF2'>
-                    <Heading fontSize='2xs' fontWeight={theme.fontWeights.bold} color={theme.colors.neutral.black}>
-                        {element.title}
-                    </Heading>
-                    <Text fontSize='13px' mt='6px' fontWeight='extrabold' color='neutral.grayDark'>
-                        {element.amount}
-                    </Text>
-                </Box>)
 
-            })}
+            <Box display='flex' flexWrap='wrap'>
+                {arr.map(element => {
+                    return (
+                        <Box ml={isMobile ? 0  : (isTablet ? '16px' : 0)} key={element.id} width={isMobile ? '100%' : (isTablet ? '46%' : '100%')} mb='12px' p='10px'
+                             border='1px solid #EDEEF2' borderRadius='16px'>
+                            <Heading fontSize='2xs' fontWeight={theme.fontWeights.bold}
+                                     color={theme.colors.neutral.black}>
+                                {element.title}
+                            </Heading>
+                            <Text fontSize='13px' mt='6px' fontWeight='extrabold' color='neutral.grayDark'>
+                                {element.amount}
+                            </Text>
+                        </Box>
+                    )
+                })}
+            </Box>
+
             <Divider mt='21px'/>
-            <Box mt='20px' display='flex' justifyContent='space-between'>
-                <Box px='5px' border='1px solid #EDEEF2' borderRadius='16px' display='flex' flex-direction='column'>
+            <Box gap={isTablet ? 4 : 0} mt='20px' display='flex' justifyContent='space-between'>
+                <Box width={isTablet ? '100%' : ''} px='5px' border='1px solid #EDEEF2'
+                     borderRadius='16px'
+                     display='flex' flex-direction='column'>
                     <Button width='95%' display='flex' flexDirection='column' h='72px'>
                         <AddPlus/>
                         <Text mt='6px'>
@@ -33,7 +47,7 @@ export default function CategoryMenu() {
                         </Text>
                     </Button>
                 </Box>
-                <Box border='1px solid #EDEEF2' borderRadius='16px'>
+                <Box width={isTablet ? '100%' : ''} border='1px solid #EDEEF2' borderRadius='16px'>
                     <Button width='95%' display='flex' flexDirection='column' h='72px'>
                         <AddPlus/>
                         <Text mt='6px'>
--- src/admin/adminComponents/AdminRestaurantMenu/ListOfProducts.jsx
+++ src/admin/adminComponents/AdminRestaurantMenu/ListOfProducts.jsx
@@ -1,4 +1,3 @@
-import React, {useState} from 'react';
 import {
     Box,
     GridItem,
@@ -7,121 +6,111 @@ import {
     Heading,
     Divider,
     Button,
-    Editable,
-    EditablePreview,
-    EditableInput
+    useBreakpointValue,
+    useMediaQuery,
 } from "@chakra-ui/react";
 import theme from "../../../utils/theme";
-
+import {arrayProducts} from '../../adminJSON/adminListOfProducts';
 import DragAndDrop from "../../../assets/svg/DragAndDrop";
 import Pen from "../../../assets/svg/Pen";
 import Copy from "../../../assets/svg/Copy";
 import TrashBox from "../../../assets/svg/TrashBox";
 
 export default function ListOfProducts() {
-    const [description, setDescription] = useState(item.description);
-    const [isEditing, setIsEditing] = useState(false);
+    const gridColumns = useBreakpointValue({base: "1fr", md: "1fr 4fr"});
+    const [isMobile] = useMediaQuery("(max-width: 576px)");
+    const [isTablet] = useMediaQuery("(max-width: 768px)");
 
-    const handleSave = () => {
-        setIsEditing(false);
-        // save the updated description to the server or to the item object
-    };
-    const array = [{
-        id: 1238,
-        image: 'https://cdn.pixabay.com/photo/2023/04/26/16/57/flower-7952897_960_720.jpg',
-        title: 'Nigiri set',
-        description: 'Lorem ipsum dolor sit amet, pri atqui facete evertitur an, ea assum solet invidunt vim.',
-        price: '$16.80',
-        ingredients: 'Lorem ipsum dolor sit amet, pri atqui facete evertitur an, ea assum solet invidunt vim.',
-        nutritionalValue: 'Proteins - 7.11, Fats - 5.17, Carbohydrates - 18.40, 146 kkal. (for 100 g.)'
-    }, {
-        id: 1237,
-        image: 'https://cdn.pixabay.com/photo/2023/04/26/16/57/flower-7952897_960_720.jpg',
-        title: 'Nigiri set',
-        description: 'Lorem ipsum dolor sit amet, pri atqui facete evertitur an, ea assum solet invidunt vim.',
-        price: '$16.80',
-        ingredients: 'Lorem ipsum dolor sit amet, pri atqui facete evertitur an, ea assum solet invidunt vim.',
-        nutritionalValue: 'Proteins - 7.11, Fats - 5.17, Carbohydrates - 18.40, 146 kkal. (for 100 g.)'
-    }, {
-        id: 1236,
-        image: 'https://cdn.pixabay.com/photo/2023/04/26/16/57/flower-7952897_960_720.jpg',
-        title: 'Nigiri set',
-        description: 'Lorem ipsum dolor sit amet, pri atqui facete evertitur an, ea assum solet invidunt vim.',
-        price: '$16.80',
-        ingredients: 'Lorem ipsum dolor sit amet, pri atqui facete evertitur an, ea assum solet invidunt vim.',
-        nutritionalValue: 'Proteins - 7.11, Fats - 5.17, Carbohydrates - 18.40, 146 kkal. (for 100 g.)'
-    }, {
-        id: 1235,
-        image: 'https://cdn.pixabay.com/photo/2023/04/26/16/57/flower-7952897_960_720.jpg',
-        title: 'Nigiri set',
-        description: 'Lorem ipsum dolor sit amet, pri atqui facete evertitur an, ea assum solet invidunt vim.',
-        price: '$16.80',
-        ingredients: 'Lorem ipsum dolor sit amet, pri atqui facete evertitur an, ea assum solet invidunt vim.',
-        nutritionalValue: 'Proteins - 7.11, Fats - 5.17, Carbohydrates - 18.40, 146 kkal. (for 100 g.)'
-    }]
-    const [value, setValue] = useState("Initial value");
+    return (
+        <GridItem colSpan={8}>
+            <Text mb='16px' fontSize='sm' fontWeight={theme.fontWeights.semibold} color='neutral.black'></Text>
+            {arrayProducts.map(item => {
+                return (
+                    <Box
+                        key={item.id}
+                        display='flex'
+                        flexDirection={isTablet ? 'column' : 'row'}
+                        mt='30px'
+                        flexWrap='wrap'
+                        borderRadius='16px'
+                        p='16px 16px 16px 12px'
+                        border='1px'
+                        borderColor='neutral.grayLightest'
+                        gap='12px'
+                        gridTemplateColumns={gridColumns}
+                    >
+                        <Box
+                            gap={3}
+                            display='flex'
+                            flexDirection={isTablet ? 'row' : 'row'}
+                            alignItems='center'
+                            mb={['16px', '16px', 0]}
+                        >
+                            <Box>
+                                <DragAndDrop/>
+                            </Box>
+                            <Box flexWrap={"wrap"} position="relative" width={isMobile ? '200px' : '100px'}
+                                 height="100px">
+                                <Image
+                                    width="100%"
+                                    height="100%"
+                                    borderRadius="20px"
+                                    src={item.image}
+                                    objectFit="cover"
+                                    objectPosition="center"
+                                />
+                            </Box>
+                            <Box display='flex' ml={[3, 3, 0]} alignItems='center'>
+                                  <Box
+                                      display='flex'
+                                      alignItems='start'
+                                      flexDirection='column'
+                                  >
+                                      <Heading fontSize='2xs' lineHeight='24px' fontWeight='bold' color='neutral.black'>
+                                          {item.title}
+                                      </Heading>
+                                      <Box fontSize='2xs'>{item.description}</Box>
+                              </Box>
+                               <Box alignItems='center' display='flex' gap={3}>
+                                   <Text fontSize='xs' fontWeight='bold' color='neutral.black'>{item.price}</Text>
+                                   <Box ml='13px' mr='12px' h='20px' w='1px' mx='4' bg='neutral.grayLightest'/>
+                                   <Button>
+                                       <Pen/>
+                                   </Button>
+                                   <Copy/>
+                                   <TrashBox/>
+                               </Box>
 
-    return (<GridItem colSpan={8}>
-        <Text mb='16px' fontSize='sm' fontWeight={theme.fontWeights.semibold} color='neutral.black'></Text>
-        {array.map(item => {
-            return (<Box key={item.id} display='flex' mt='30px' borderRadius='16px' p='16px 16px 16px 12px' border='1px'
-                         borderColor='neutral.grayLightest' gap='12px'>
-                <Box display='flex' alignItems='center'>
-                    <Box>
-                        <DragAndDrop/>
-                    </Box>
-                    <Box position="relative" width="100px" height="100px">
-                        <Image
-                            width="100%"
-                            height="100%"
-                            borderRadius="20px"
-                            src={item.image}
-                            objectFit="cover"
-                            objectPosition="center"
-                        />
-                    </Box>
-                </Box>
-
-                <Box>
-                    <Box display='flex' alignItems='center' gap={4}>
-                        <Box alignItems='center'>
-                            <Heading fontSize='2xs' lineHeight='24px' fontWeight='bold' color='neutral.black'>
-                                {item.title}
-                            </Heading>
-                            {isEditing ? (
-                                <Editable value={description} fontSize='2xs' onChange={setDescription}>
-                                    <Box>{description}</Box>
-                                </Editable>
-                            ) : (
-                                <Box fontSize='2xs'>{description}</Box>
-                            )}
-                        </Box>
-
-                        <Box display='flex' alignItems='center' lineHeight='30px' gap={3}>
-                            <Text fontSize='xs' fontWeight='bold' color='neutral.black'>{item.price}</Text>
-                            <Box ml='13px' mr='12px' h='20px' w='1px' mx='4' bg='neutral.grayLightest'/>
-                            <Button onClick={() => setIsEditing(true)}>
-                                <Pen/>
-                            </Button>
-                            <Copy/>
-                            <TrashBox/>
-                        </Box>
-                    </Box>
-                    <Divider mt='20px' mb='16px'/>
-                    <Box display='flex' justifyContent='space-between'>
-                        <Box>
-                            <Heading fontSize='2xs' lineHeight='24px'
-                                     fontWeight='bold' color='neutral.black'>Ingredients</Heading>
-                            <Text fontSize='13px' color='neutral.grayDark'>{item.ingredients}</Text>
+                            </Box>
                         </Box>
                         <Box>
-                            <Heading fontSize='2xs' lineHeight='24px'
-                                     fontWeight='bold'>Nutritional value</Heading>
-                            <Text fontSize='13px' color='neutral.grayDark'>{item.nutritionalValue}</Text>
+                            <Box
+                                display='flex'
+                                flexDirection={['row', 'row', 'row-reverse']}
+                                alignItems='center'
+                                justifyContent='space-around'
+
+                                gap={4}
+                                flexWrap='wrap'
+                            >
+                            </Box>
+                            <Divider mt='20px' mb='16px'/>
+
+                            <Box display='flex' flexDirection={isTablet ? 'row' : 'row'} justifyContent='space-between'>
+
+                                <Box mb={['16px', '16px', 0]}>
+                                    <Heading fontSize='2xs' lineHeight='24px'
+                                             fontWeight='bold' color='neutral.black'>Ingredients</Heading>
+                                    <Text fontSize='13px' color='neutral.grayDark'>{item.ingredients}</Text>
+                                </Box>
+                                <Box>
+                                    <Heading fontSize='2xs' lineHeight='24px'
+                                             fontWeight='bold'>Nutritional value</Heading>
+                                    <Text fontSize='13px' color='neutral.grayDark'>{item.nutritionalValue}</Text>
+                                </Box>
+                            </Box>
                         </Box>
-                    </Box>
-                </Box>
-            </Box>)
-        })}
-    </GridItem>);
+                    </Box>)
+            })}
+        </GridItem>)
 }
