c74c801f2cc295b428efec1f1664c799cde74178
brndalx <brndalx@gmail.com>
2023-05-10 03:28:32 +0300

    feat: security change password section

--- src/admin/adminComponents/RestaurantSettings/Security.jsx
+++ src/admin/adminComponents/RestaurantSettings/Security.jsx
@@ -66,7 +66,83 @@ export default function Security() {
               </Button>
             </Flex>
           </Box>
-          <Box pt={5}>box2</Box>
+          <Text pt='40px' fontSize='xs' fontWeight='bold' color='neutral.black'>
+            Change password
+          </Text>
+          <Box pt={5}>
+            <Flex flexDirection={{ base: 'column', md: 'row' }} alignItems={{ base: 'normal', md: 'flex-end' }}>
+              <FormControl id='curpassword' mt={{ base: 5, md: 0 }}>
+                <FormLabel fontWeight='semibold' fontSize='3xs' color='neutral.grayDark'>
+                  Current password
+                </FormLabel>
+
+                <Input
+                  w={{ base: '100%', md: '90%' }}
+                  type='password'
+                  background='neutral.white'
+                  _placeholder={{ color: 'neutral.gray' }}
+                  borderRadius='8px'
+                  fontSize='2xs'
+                  placeholder='Enter current password'
+                />
+              </FormControl>
+
+              <FormControl id='newpassword' mt={{ base: 5, md: 0 }}>
+                <FormLabel fontWeight='semibold' fontSize='3xs' color='neutral.grayDark'>
+                  New password
+                </FormLabel>
+
+                <Input
+                  w={{ base: '100%', md: '90%' }}
+                  type='password'
+                  background='neutral.white'
+                  _placeholder={{ color: 'neutral.gray' }}
+                  borderRadius='8px'
+                  fontSize='2xs'
+                  placeholder='Enter new password'
+                />
+              </FormControl>
+              <FormControl id='conpassword' mt={{ base: 5, md: 0 }}>
+                <FormLabel fontWeight='semibold' fontSize='3xs' color='neutral.grayDark'>
+                  Confirm new password
+                </FormLabel>
+
+                <Input
+                  w={{ base: '100%', md: '90%' }}
+                  type='password'
+                  background='neutral.white'
+                  _placeholder={{ color: 'neutral.gray' }}
+                  borderRadius='8px'
+                  fontSize='2xs'
+                  placeholder='Confirm new password'
+                />
+              </FormControl>
+            </Flex>
+          </Box>
+
+          <Box pt={5}>
+            <Flex justifyContent='flex-end'>
+              <Button
+                mt={{ base: 5, md: 0 }}
+                w={{ base: '100%', md: 'initial' }}
+                background='bg'
+                fontWeight='bold'
+                variant='solid'
+                color='neutral.grayLight'
+                borderWidth='1px'
+                borderColor='bg'
+                _hover={{
+                  background: 'neutral.white',
+                  color: 'primary.default',
+                  borderWidth: '1px',
+                  borderColor: 'primary.default'
+                }}
+                py={5}
+              >
+                Change password
+              </Button>
+            </Flex>
+          </Box>
         </Box>
       </Box>
     </>
