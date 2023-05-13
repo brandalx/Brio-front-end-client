bb609682c52f00d6cf96926f3eddb042601eadb6
brndalx <brndalx@gmail.com>
2023-05-14 02:11:43 +0300

    feat: react chart js basic craph layout

--- src/admin/adminPages/RestaurantDashboard.jsx
+++ src/admin/adminPages/RestaurantDashboard.jsx
@@ -1,57 +1,54 @@
-import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../../assets/svg/Logo';
-import Emoji from 'react-emojis';
+import { Box } from '@chakra-ui/layout';
+import {
+  Filler,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler);
 
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top'
+    },
+    title: {
+      display: true,
+      text: 'Order revenue'
+    }
+  }
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Restaurant activity',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 10000 })),
+      borderColor: '#8E99FF',
+      backgroundColor: 'rgba(175, 183, 255, 0.21)',
+      fill: 'origin',
+      tension: 0.3
+    }
+  ]
+};
 export default function RestaurantDashboard() {
   return (
     <Box display='flex' justifyContent='center' alignItems='center' height='90vh'>
-      <Box
-        textAlign='center'
-        w='50%' // set width to 50%
-      >
-        <Box>
-          <Box>
-            <Flex alignItems='center' justifyContent='center'>
-              {' '}
-              <Logo />
-              <Text fontSize='sm' fontWeight='extrabold' color='primary.default' ml='1'>
-                Brio
-              </Text>
-            </Flex>
-          </Box>{' '}
-        </Box>
-
-        <Text fontSize='2xl' fontWeight='extrabold' color='primary.default' lineHeight='55px'>
-          Admin Dashboard
-        </Text>
-        <Text fontSize='sm' fontWeight='BOLD' color='neutral.grayDark'>
-          We're working on this page <Emoji emoji='construction' size='20' />
-        </Text>
-        <Link to='/'>
-          <Button
-            mt={5}
-            px={5}
-            borderRadius={100}
-            background='primary.default'
-            fontWeight='bold'
-            variant='solid'
-            color='neutral.white'
-            borderWidth='1px'
-            borderColor='neutral.white'
-            _hover={{
-              background: 'neutral.white',
-              color: 'primary.default',
-              borderWidth: '1px',
-              borderColor: 'primary.default'
-            }}
-            py={5}
-          >
-            Back to home
-          </Button>
-        </Link>
+      <Box textAlign='center' w='90%'>
+        <Line options={options} data={data} />
       </Box>
     </Box>
   );
