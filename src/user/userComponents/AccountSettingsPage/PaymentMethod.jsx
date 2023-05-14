2ad497e88f62df47e591130b2e7ba305a092ae11
brndalx <brndalx@gmail.com>
2023-05-14 23:47:21 +0300

    feat: new payment method layout

--- src/user/userComponents/AccountSettingsPage/PaymentMethod.jsx
+++ src/user/userComponents/AccountSettingsPage/PaymentMethod.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Grid, Text } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, Grid, GridItem, Input, Text } from '@chakra-ui/react';
 
 import PaymentCard from './PaymentCard';
 import visa from '../../../assets/images/visa.png';
@@ -54,6 +54,107 @@ export default function PaymentMethod() {
                 return <PaymentCard key={index} item={item} />;
               })}
             </Grid>
+          </Box>
+          <Box pt={5}>
+            <Text mb='16px' fontSize='xs' fontWeight='bold' color='neutral.black'>
+              New payment method
+            </Text>
+            <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: '1fr 1fr 1fr ' }} gap={6}>
+              <GridItem w='100%'>
+                <FormControl id='number'>
+                  <FormLabel fontWeight='semibold' fontSize='3xs' color='neutral.grayDark'>
+                    Card number
+                  </FormLabel>
+
+                  <Input
+                    type='text'
+                    background='neutral.white'
+                    _placeholder={{ color: 'neutral.gray' }}
+                    borderRadius='8px'
+                    fontSize='2xs'
+                    placeholder='XXXX - XXXX - XXXX - XXXX'
+                  />
+                </FormControl>
+              </GridItem>
+              <GridItem w='100%'>
+                <FormControl id='exp'>
+                  <FormLabel fontWeight='semibold' fontSize='3xs' color='neutral.grayDark'>
+                    Expiration
+                  </FormLabel>
+
+                  <Input
+                    type='text'
+                    background='neutral.white'
+                    _placeholder={{ color: 'neutral.gray' }}
+                    borderRadius='8px'
+                    fontSize='2xs'
+                    placeholder='MM / YYYY'
+                  />
+                </FormControl>
+              </GridItem>
+              <GridItem w='100%'>
+                <FormControl id='cvc'>
+                  <FormLabel
+                    fontWeight='semibold'
+                    placeholder='+1(217) 555-0113'
+                    fontSize='3xs'
+                    color='neutral.grayDark'
+                  >
+                    CVC
+                  </FormLabel>
+
+                  <Input
+                    type='text'
+                    background='neutral.white'
+                    _placeholder={{ color: 'neutral.gray' }}
+                    borderRadius='8px'
+                    fontSize='2xs'
+                    placeholder='XXX'
+                  />
+                </FormControl>
+              </GridItem>
+            </Grid>
+          </Box>
+          <Box pt={5}>
+            <Grid templateColumns='repeat(1, 1fr)' gap={4}>
+              <GridItem w='100%'>
+                <FormControl id='cardholder'>
+                  <FormLabel fontWeight='semibold' fontSize='3xs' color='neutral.grayDark'>
+                    Cardholder
+                  </FormLabel>
+
+                  <Input
+                    type='text'
+                    background='neutral.white'
+                    _placeholder={{ color: 'neutral.gray' }}
+                    borderRadius='8px'
+                    fontSize='2xs'
+                    placeholder='Enter name on card'
+                  />
+                </FormControl>
+              </GridItem>
+            </Grid>
+          </Box>
+          <Box pt={5} display='flex' justifyContent='flex-end'>
+            <Button
+              background='neutral.white'
+              fontSize='2xs'
+              fontWeight='bold'
+              variant='solid'
+              color='primary.default'
+              borderWidth='1px'
+              borderColor='primary.default'
+              _hover={{
+                background: 'primary.default',
+                color: 'neutral.white',
+                borderWidth: '1px',
+                borderColor: 'primary.default'
+              }}
+              py={5}
+              me='20px'
+            >
+              Add new payment method
+            </Button>
           </Box>
         </Box>
       </Box>
