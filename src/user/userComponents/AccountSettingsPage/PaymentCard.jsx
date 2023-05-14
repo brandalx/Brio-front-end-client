461b9923284639333a692e97256e55b7a8c7f172
brndalx <brndalx@gmail.com>
2023-05-14 23:34:43 +0300

    feat: connected payment methods cards

---
+++ src/user/userComponents/AccountSettingsPage/PaymentCard.jsx
@@ -0,0 +1,96 @@
+import {
+  Box,
+  Flex,
+  GridItem,
+  Image,
+  Heading,
+  Menu,
+  Text,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  MenuButton,
+  Avatar
+} from '@chakra-ui/react';
+import React from 'react';
+import imagemap from '../../../assets/images/defaultmap.png';
+export default function PaymentCard({ item }) {
+  return (
+    <GridItem w='100%'>
+      <Box
+        _hover={{
+          cursor: 'pointer',
+          transition: 'all 0.3s',
+          bg: 'primary.light',
+          borderColor: 'primary.light'
+        }}
+        _active={{
+          cursor: 'pointer',
+          transition: 'all 0.3s',
+          bg: 'primary.light',
+          borderColor: 'primary.default'
+        }}
+        borderRadius='16px'
+        mb='12px'
+        p='15px'
+        transition='all 0.3s'
+        borderWidth='1px'
+        bg='neutral.white'
+      >
+        <Flex justifyContent='space-between'>
+          <Flex alignItems='center'>
+            <Box>
+              <Heading fontSize='2xs' fontWeight='bold'>
+                {item.number}
+              </Heading>
+              <Text fontSize='3xs' fontWeight='semibold' color='neutral.grayDark'>
+                {item.expiration}
+              </Text>
+              <Text fontSize='3xs' color='neutral.grayDark'>
+                {item.cardholder}
+              </Text>
+            </Box>
+          </Flex>
+          <Box display='flex' flexDir='column' justifyContent='space-between' alignItems='flex-end'>
+            <Menu>
+              <MenuButton
+                _hover={{
+                  color: 'neutral.black',
+                  borderColor: 'neutral.lightest'
+                }}
+                fontSize='2xs'
+                color='neutral.gray'
+                fontWeight='bold'
+                py={0}
+                me='0px'
+              >
+                •••
+              </MenuButton>
+
+              <MenuList>
+                <MenuItem fontWeight='medium'>Edit</MenuItem>
+                <MenuDivider />
+                <MenuItem
+                  m={0}
+                  h='100%'
+                  background='neutral.white'
+                  variant='solid'
+                  color='error.default'
+                  _hover={{
+                    background: 'error.default',
+                    color: 'neutral.white'
+                  }}
+                  fontWeight='medium'
+                >
+                  {' '}
+                  Remove
+                </MenuItem>
+              </MenuList>
+            </Menu>
+            <Image w='100%' h='25px' src={item.cardtype} />
+          </Box>
+        </Flex>
+      </Box>
+    </GridItem>
+  );
+}
--- src/user/userComponents/AccountSettingsPage/PaymentMethod.jsx
+++ src/user/userComponents/AccountSettingsPage/PaymentMethod.jsx
@@ -13,9 +13,52 @@ import {
   Textarea,
   Stack,
   Checkbox,
-  Divider
+  Divider,
+  Heading,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider
 } from '@chakra-ui/react';
+import AdressCard from './AdressCard';
+import PaymentCard from './PaymentCard';
+import visa from '../../../assets/images/visa.png';
+import mastercard from '../../../assets/images/mastercard.png';
 export default function PaymentMethod() {
+  let arr = [
+    {
+      number: '**** **** **** 4629',
+      expiration: '10/23',
+      cardholder: 'Jane Robertson',
+      cardtype: mastercard
+    },
+    {
+      number: '**** **** **** 6789',
+      expiration: '05/24',
+      cardholder: 'John Smith',
+      cardtype: visa
+    },
+    {
+      number: '**** **** **** 1234',
+      expiration: '12/25',
+      cardholder: 'Sarah Johnson',
+      cardtype: mastercard
+    },
+    {
+      number: '**** **** **** 9876',
+      expiration: '03/26',
+      cardholder: 'Michael Davis',
+      cardtype: visa
+    },
+    {
+      number: '**** **** **** 5555',
+      expiration: '08/27',
+      cardholder: 'Emily Thompson',
+      cardtype: visa
+    }
+  ];
+
   return (
     <>
       <Box>
@@ -26,8 +69,13 @@ export default function PaymentMethod() {
           <Text fontSize='xs' fontWeight='bold' color='neutral.black'>
             Connected payment methods
           </Text>
-          <Box pt={5}></Box>
-          Payment method content
+          <Box pt={5}>
+            <Grid templateColumns={{ base: 'repeat(1, 1fr)', lg: '1fr 1fr ' }} gap={6}>
+              {arr.map((item, index) => {
+                return <PaymentCard key={index} item={item} />;
+              })}
+            </Grid>
+          </Box>
         </Box>
       </Box>
     </>
