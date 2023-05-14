4f60b9e6624c1258936e5970609e98b620d0c9f1
brndalx <brndalx@gmail.com>
2023-05-14 20:57:36 +0300

    feat: adress cards array

--- src/user/userComponents/AccountSettingsPage/Address.jsx
+++ src/user/userComponents/AccountSettingsPage/Address.jsx
@@ -24,6 +24,37 @@ import {
 import imagemap from '../../../assets/images/defaultmap.png';
 import { Link } from 'react-router-dom';
 export default function Adress() {
+  let arr = [
+    {
+      country: 'USA',
+      state: 'New York',
+      city: 'New York',
+      address1: '4517 Washington Ave.',
+      address2: 'Manchester, 11004'
+    },
+    {
+      country: 'USA',
+      state: 'New York',
+      city: 'New York',
+      address1: '123 Broadway',
+      address2: 'New York, 10001'
+    },
+    {
+      country: 'USA',
+      state: 'New York',
+      city: 'Brooklyn',
+      address1: '789 Elm Street',
+      address2: 'Brooklyn, 11201'
+    },
+    {
+      country: 'USA',
+      state: 'New York',
+      city: 'Albany',
+      address1: '456 Oak Avenue',
+      address2: 'Albany, 12207'
+    }
+  ];
+
   return (
     <>
       <Box>
@@ -36,94 +67,97 @@ export default function Adress() {
           </Text>
           <Box pt={5}>
             <Grid templateColumns={{ base: 'repeat(1, 1fr)', lg: '1fr 1fr ' }} gap={6}>
-              <GridItem w='100%'>
-                <Box
-                  _hover={{
-                    cursor: 'pointer',
-                    transition: 'all 0.3s',
-                    bg: 'primary.light',
-                    borderColor: 'primary.light'
-                  }}
-                  _active={{
-                    cursor: 'pointer',
-                    transition: 'all 0.3s',
-                    bg: 'primary.light',
-                    borderColor: 'primary.default'
-                  }}
-                  borderRadius='16px'
-                  mb='12px'
-                  p='0px'
-                  transition='all 0.3s'
-                  borderWidth='1px'
-                  bg='neutral.white'
-                >
-                  <Flex justifyContent='space-between'>
-                    <Flex alignItems='center'>
-                      <Box py={2} px={2} borderRadius={12}>
-                        <Box ml='4px' py={'7.5px'} position='relative'>
-                          <Box>
-                            <Image maxH='82px' maxW='96px' borderRadius='12px' src={imagemap} />
+              {arr.map((item, index) => {
+                return (
+                  <GridItem key={index} w='100%'>
+                    <Box
+                      _hover={{
+                        cursor: 'pointer',
+                        transition: 'all 0.3s',
+                        bg: 'primary.light',
+                        borderColor: 'primary.light'
+                      }}
+                      _active={{
+                        cursor: 'pointer',
+                        transition: 'all 0.3s',
+                        bg: 'primary.light',
+                        borderColor: 'primary.default'
+                      }}
+                      borderRadius='16px'
+                      mb='12px'
+                      p='0px'
+                      transition='all 0.3s'
+                      borderWidth='1px'
+                      bg='neutral.white'
+                    >
+                      <Flex justifyContent='space-between'>
+                        <Flex alignItems='center'>
+                          <Box py={2} px={2} borderRadius={12}>
+                            <Box ml='4px' py={'7.5px'} position='relative'>
+                              <Box>
+                                <Image maxH='82px' maxW='96px' borderRadius='12px' src={imagemap} />
+                              </Box>
+                            </Box>
                           </Box>
+                          <Box>
+                            <Heading fontSize='2xs' fontWeight='bold'>
+                              {item.city}
+                            </Heading>
+                            <Text fontSize='3xs' fontWeight='semibold' color='neutral.grayDark'>
+                              {item.state} State, {item.country}
+                            </Text>
+                            <Text fontSize='3xs' color='neutral.grayDark'>
+                              {item.address1} {item.address2}
+                            </Text>
+                          </Box>
+                        </Flex>
+                        <Box>
+                          <Menu>
+                            <MenuButton
+                              _hover={{
+                                color: 'neutral.black',
+
+                                borderColor: 'neutral.lightest'
+                              }}
+                              fontSize='2xs'
+                              color='neutral.gray'
+                              fontWeight='bold'
+                              py={5}
+                              me='20px'
+                            >
+                              •••
+                            </MenuButton>
+
+                            <MenuList>
+                              <Link to='/user/account'>
+                                {' '}
+                                <MenuItem fontWeight='medium'>Edit</MenuItem>
+                              </Link>
+
+                              <MenuDivider />
+                              <MenuItem
+                                m={0}
+                                h='100%'
+                                background='neutral.white'
+                                variant='solid'
+                                color='error.default'
+                                _hover={{
+                                  background: 'error.default',
+                                  color: 'neutral.white'
+                                }}
+                                fontWeight='medium'
+                              >
+                                {' '}
+                                Remove
+                              </MenuItem>
+                            </MenuList>
+                          </Menu>
                         </Box>
-                      </Box>
-                      <Box>
-                        <Heading fontSize='2xs' fontWeight='bold'>
-                          New York
-                        </Heading>
-                        <Text fontSize='3xs' fontWeight='semibold' color='neutral.grayDark'>
-                          New York State, USA
-                        </Text>
-                        <Text fontSize='3xs' color='neutral.grayDark'>
-                          4517 Washington Ave. Manchester, 11004
-                        </Text>
-                      </Box>
-                    </Flex>
-                    <Box>
-                      <Menu>
-                        <MenuButton
-                          _hover={{
-                            color: 'neutral.black',
-
-                            borderColor: 'neutral.lightest'
-                          }}
-                          fontSize='2xs'
-                          color='neutral.gray'
-                          fontWeight='bold'
-                          py={5}
-                          me='20px'
-                        >
-                          •••
-                        </MenuButton>
-
-                        <MenuList>
-                          <Link to='/user/account'>
-                            {' '}
-                            <MenuItem fontWeight='medium'>Edit</MenuItem>
-                          </Link>
-
-                          <MenuDivider />
-                          <MenuItem
-                            m={0}
-                            h='100%'
-                            background='neutral.white'
-                            variant='solid'
-                            color='error.default'
-                            _hover={{
-                              background: 'error.default',
-                              color: 'neutral.white'
-                            }}
-                            fontWeight='medium'
-                          >
-                            {' '}
-                            Remove
-                          </MenuItem>
-                        </MenuList>
-                      </Menu>
+                      </Flex>
                     </Box>
-                  </Flex>
-                </Box>
-              </GridItem>
-              <GridItem w='100%'>2</GridItem>
+                  </GridItem>
+                );
+              })}
             </Grid>
           </Box>
         </Box>
