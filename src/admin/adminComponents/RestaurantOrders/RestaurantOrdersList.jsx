ffdd1b7e10ad5105a2e4776249e8e4652f7f9230
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-05-14 12:07:28 +0300

    Feat: rows settled in a proper way

--- src/admin/adminComponents/RestaurantOrders/RestaurantOrdersList.jsx
+++ src/admin/adminComponents/RestaurantOrders/RestaurantOrdersList.jsx
@@ -2,35 +2,37 @@ import { Box, Container, Flex, Table, TableCaption, TableContainer, Tbody, Td, T
 import React from 'react';
 import Status from '../../../assets/svg/Status';
 import { arrUsers } from '../../adminJSON/adminListOfUsers';
+import ThreeDots from '../../../assets/svg/ThreeDots';
 
 export default function RestaurantOrdersList() {
     return (
-        <Container maxW="1100px" pb="50px">
-            <TableContainer overflowX="hidden">
-                <Table colorScheme="gray">
+        <Container maxW="1132px" pb="50px">
+            <TableContainer overflowX="hidden" borderWidth="1px" borderRadius="md" borderColor="gray.200">
+                <Table size='sm' variant='simple' colorScheme="gray" borderWidth="1px" borderRadius="md" borderColor="gray.200">
                     <Thead>
                         <Tr>
-                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold">
+                            <Th pt='19.5px' pb='19.5px' color="neutral.gray" fontSize="2.5xs" fontWeight="bold" textTransform="none">
                                 Order ID
                             </Th>
-                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold">
+                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" textTransform="none">
                                 Customer
                             </Th>
-                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" isNumeric>
+                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" textTransform="none">
                                 Address
                             </Th>
-                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" isNumeric>
+                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" textTransform="none">
                                 Creation date
                             </Th>
-                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" isNumeric>
+                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" textTransform="none">
                                 Creation time
                             </Th>
-                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" isNumeric>
+                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" textTransform="none">
                                 Status
                             </Th>
-                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" isNumeric>
+                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" textTransform="none" isNumeric>
                                 Total amount
                             </Th>
+                            <Th color="neutral.gray" fontSize="2.5xs" fontWeight="bold" isNumeric></Th>
                         </Tr>
                     </Thead>
                     <Tbody>
@@ -41,22 +43,22 @@ export default function RestaurantOrdersList() {
                                     transition="all 0.2s"
                                     _hover={{ bg: 'bg', transition: 'all 0.2s', cursor: 'pointer' }}
                                 >
-                                    <Td fontSize="2xs" color="neutral.grayDark">
+                                    <Td pt='19.5px' pb='19.5px' fontSize="2xs" color="neutral.grayDark">
                                         {item.orderId}
                                     </Td>
-                                    <Td fontSize="2xs" color="neutral.grayDark" fontWeight="semibold">
+                                    <Td pt='19.5px' pb='19.5px' fontSize="2xs" color="neutral.grayDark" fontWeight="semibold">
                                         {item.name}
                                     </Td>
-                                    <Td fontSize="2xs" color="neutral.grayDark" isNumeric>
+                                    <Td pt='19.5px' pb='19.5px' fontSize="2xs" color="neutral.grayDark" >
                                         {item.address}
                                     </Td>
-                                    <Td fontSize="2xs" color="neutral.grayDark" isNumeric>
+                                    <Td pt='19.5px' pb='19.5px' fontSize="2xs" color="neutral.grayDark" >
                                         {item.creationDate}
                                     </Td>
-                                    <Td fontSize="2xs" color="neutral.grayDark" isNumeric>
+                                    <Td pt='19.5px' pb='19.5px' fontSize="2xs" color="neutral.grayDark" >
                                         {item.creationTime}
                                     </Td>
-                                    <Td fontSize="2.5xs" color="neutral.black" fontWeight="semibold">
+                                    <Td pt='10px' pb='10px' fontSize="2.5xs" color="neutral.black" fontWeight="semibold">
                                         <Flex alignItems="center">
                                             <Box as="span" me={2}>
                                                 <Status
@@ -76,8 +78,12 @@ export default function RestaurantOrdersList() {
                                             {item.status}
                                         </Flex>
                                     </Td>
-                                    <Td fontSize="2xs" fontWeight="bold" color="neutral.black" isNumeric>
+                                    <Td textAlign="center" pt='10px' pb='10px' fontSize="2xs" fontWeight="bold" color
+                                        ="neutral.black" >
                                         {item.totalAmount}
+                                    </Td>
+                                    <Td pl={0} pr={0} pt='10px' pb='10px' fontSize="2xs" fontWeight="bold" color="neutral.black">
+                                        <ThreeDots />
                                     </Td>
                                 </Tr>
                             );
