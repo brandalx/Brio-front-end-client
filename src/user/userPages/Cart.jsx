109e4a11b700da449b7739f0b597c93009a90230
brndalx <brndalx@gmail.com>
2023-05-15 13:09:42 +0300

    feat: basic components for checkout process cart

--- src/user/userPages/Cart.jsx
+++ src/user/userPages/Cart.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Box, Container, Grid, GridItem, Text, Divider } from '@chakra-ui/react';
+import { Box, Container, Grid, GridItem, Text, Divider, Button } from '@chakra-ui/react';
 import salad from '../../assets/images/salad.jpg';
 import MenuMeal from '../userComponents/Cart/MenuMeal';
+import Delivery from '../userComponents/Cart/Delivery';
+import { Link, Route, Routes } from 'react-router-dom';
+import Pickup from '../userComponents/Cart/Pickup';
+import Summary from '../userComponents/Cart/Summary';
 
 export default function Cart() {
   let arr = [
@@ -58,7 +62,65 @@ export default function Cart() {
               </Box>
             </Box>
           </GridItem>
-          <GridItem w='100%'>jjj</GridItem>
+          <GridItem w='100%'>
+            <Box borderRadius='16px' borderWidth='1px' py='20px' px='10px'>
+              <Box bg='neutral.grayLightest' p={2} borderRadius='16px'>
+                <Grid templateColumns='repeat(2, 1fr)' gap={2}>
+                  <GridItem w='100%'>
+                    <Link to='/user/cart' w='100%'>
+                      <Button
+                        borderRadius='14px'
+                        w='100%'
+                        background='neutral.black'
+                        fontWeight='bold'
+                        variant='solid'
+                        color='neutral.white'
+                        borderWidth='1px'
+                        borderColor='neutral.black'
+                        _hover={{
+                          background: 'neutral.white',
+                          color: 'neutral.black',
+                          borderWidth: '1px',
+                          borderColor: 'neutral.black'
+                        }}
+                        py={6}
+                      >
+                        Delivery
+                      </Button>
+                    </Link>
+                  </GridItem>
+                  <GridItem w='100%'>
+                    <Link to='/user/cart/pickup' w='100%'>
+                      <Button
+                        borderRadius='14px'
+                        w='100%'
+                        background='neutral.black'
+                        fontWeight='bold'
+                        variant='solid'
+                        color='neutral.white'
+                        borderWidth='1px'
+                        borderColor='neutral.black'
+                        _hover={{
+                          background: 'neutral.white',
+                          color: 'neutral.black',
+                          borderWidth: '1px',
+                          borderColor: 'neutral.black'
+                        }}
+                        py={6}
+                      >
+                        Pickup
+                      </Button>
+                    </Link>
+                  </GridItem>
+                </Grid>
+              </Box>
+              <Routes>
+                <Route path='/' element={<Delivery />} />
+                <Route path='/pickup' element={<Pickup />} />
+              </Routes>
+              <Summary />
+            </Box>
+          </GridItem>
         </Grid>
       </Container>
     </Box>
