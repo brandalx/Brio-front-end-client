9676c943d9b381c11657cafa4100a11b63bbb43a
brndalx <brndalx@gmail.com>
2023-05-19 22:09:51 +0300

    feat: handle api GET request method service

---
+++ src/services/apiServices.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export const API_URL = 'http://localhost:3001';
+//maintainable for fast change
+
+export const TOKEN_KEY = 'brio-token';
+//to-do : change for another token / add others
+
+export const handelApiGet = async (_url) => {
+  // headers: -> sends through a token that sits in the local storage
+  // Even if there is no local storage information, we will not get an error
+  try {
+    const resp = await axios({
+      url: _url,
+      headers: {
+        'x-api-key': localStorage[TOKEN_KEY]
+      }
+    });
+    return resp.data;
+  } catch (err) {
+    // throw -> reject is equivalent to error
+    // So we know there is a problem with the request
+    throw err;
+  }
+};
