9fd8b9b55579129d7d534f4da1b0d867da97a1e3
brndalx <113251192+brandalx@users.noreply.github.com>
2023-05-22 01:16:38 +0300

    feat: delivery cart addresses cards

--- src/user/userComponents/Cart/Delivery.jsx
+++ src/user/userComponents/Cart/Delivery.jsx
@@ -1,19 +1,7 @@
-import {
-  Box,
-  Flex,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  Text,
-  Image,
-  Heading,
-  Button
-} from '@chakra-ui/react';
+import { Box, Text, Button } from '@chakra-ui/react';
 import React from 'react';
-import imagemap from '../../../assets/images/defaultmap.png';
-import ThreeDots from '../../../assets/svg/ThreeDots';
+
+import AdressCard from '../AccountSettingsPage/AdressCard';
 export default function Delivery({ item }) {
   return (
     <Box py={4}>
@@ -21,91 +9,10 @@ export default function Delivery({ item }) {
         Select your shipping adress
       </Text>
       <Box>
-        {item.map((item, index) => {
-          return (
-            <Box key={index}>
-              <Box
-                _hover={{
-                  cursor: 'pointer',
-                  transition: 'all 0.3s',
-                  bg: 'primary.light',
-                  borderColor: 'primary.light'
-                }}
-                _active={{
-                  cursor: 'pointer',
-                  transition: 'all 0.3s',
-                  bg: 'primary.light',
-                  borderColor: 'primary.default'
-                }}
-                borderRadius='16px'
-                mb='12px'
-                p='0px'
-                transition='all 0.3s'
-                borderWidth='1px'
-                bg='neutral.white'
-              >
-                <Flex justifyContent='space-between'>
-                  <Flex alignItems='center'>
-                    <Box py={2} px={2} borderRadius={12}>
-                      <Box ml='4px' py={'7.5px'} position='relative'>
-                        <Box>
-                          <Image maxH='82px' maxW='96px' borderRadius='12px' src={imagemap} />
-                        </Box>
-                      </Box>
-                    </Box>
-                    <Box>
-                      <Heading fontSize='2xs' fontWeight='bold'>
-                        {item.city}
-                      </Heading>
-                      <Text fontSize='3xs' fontWeight='semibold' color='neutral.grayDark'>
-                        {item.state} State, {item.country}
-                      </Text>
-                      <Text fontSize='3xs' color='neutral.grayDark'>
-                        {item.address1} {item.address2}
-                      </Text>
-                    </Box>
-                  </Flex>
-                  <Box>
-                    <Menu>
-                      <MenuButton
-                        mt={4}
-                        me={3}
-                        _hover={{
-                          color: 'neutral.black',
-                          borderColor: 'neutral.lightest'
-                        }}
-                        fontSize='2xs'
-                        color='neutral.gray'
-                        fontWeight='bold'
-                      >
-                        <ThreeDots />
-                      </MenuButton>
-
-                      <MenuList>
-                        <MenuItem fontWeight='medium'>Edit</MenuItem>
-                        <MenuDivider />
-                        <MenuItem
-                          m={0}
-                          background='neutral.white'
-                          variant='solid'
-                          color='error.default'
-                          _hover={{
-                            background: 'error.default',
-                            color: 'neutral.white'
-                          }}
-                          fontWeight='medium'
-                        >
-                          {' '}
-                          Remove
-                        </MenuItem>
-                      </MenuList>
-                    </Menu>
-                  </Box>
-                </Flex>
-              </Box>
-            </Box>
-          );
-        })}
+        {item &&
+          item.map((item, index) => {
+            return <AdressCard key={index} item={item} index={index} />;
+          })}
       </Box>
       <Button
         w='100%'
