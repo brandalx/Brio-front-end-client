be714b6e8196d4f14704d3fedde26d2eba27b57e
brndalx <113251192+brandalx@users.noreply.github.com>
2023-05-24 13:00:54 +0300

    feat: paymentmethod post data to account

--- src/user/userComponents/AccountSettingsPage/PaymentMethod.jsx
+++ src/user/userComponents/AccountSettingsPage/PaymentMethod.jsx
@@ -5,6 +5,7 @@ import PaymentCard from './PaymentCard';
 import visa from '../../../assets/images/visa.png';
 import mastercard from '../../../assets/images/mastercard.png';
 import { API_URL, handelApiGet } from '../../../services/apiServices';
+import NewPaymentMethod from '../Checkout/NewPaymentMethod';
 export default function PaymentMethod() {
   const [loading, setLoading] = useState(true);
   const [arr, setArr] = useState([]);
@@ -32,6 +33,10 @@ export default function PaymentMethod() {
       setLoading(false);
       console.log(error);
     }
+  };
+
+  const updateCreditCard = (newCardData) => {
+    setCardsArr((prevCardsArr) => [...prevCardsArr, newCardData]);
   };
 
   useEffect(() => {
@@ -69,126 +74,7 @@ export default function PaymentMethod() {
               </Grid>
             )}
           </Box>
-          <Box pt={5}>
-            <Text mb='16px' fontSize='xs' fontWeight='bold' color='neutral.black'>
-              New payment method
-            </Text>
-            <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: '1fr 1fr 1fr ' }} gap={6}>
-              <GridItem w='100%'>
-                <FormControl id='number'>
-                  <FormLabel fontWeight='semibold' fontSize='3xs' color='neutral.grayDark'>
-                    Card number
-                  </FormLabel>
-
-                  <Input
-                    type='password'
-                    background='neutral.white'
-                    _placeholder={{ color: 'neutral.gray' }}
-                    borderRadius='8px'
-                    fontSize='2xs'
-                    placeholder='XXXX - XXXX - XXXX - XXXX'
-                  />
-                </FormControl>
-              </GridItem>
-              <GridItem w='100%'>
-                <FormControl id='exp'>
-                  <FormLabel fontWeight='semibold' fontSize='3xs' color='neutral.grayDark'>
-                    Expiration
-                  </FormLabel>
-
-                  <Input
-                    type='text'
-                    background='neutral.white'
-                    _placeholder={{ color: 'neutral.gray' }}
-                    borderRadius='8px'
-                    fontSize='2xs'
-                    placeholder='MM / YYYY'
-                  />
-                </FormControl>
-              </GridItem>
-              <GridItem w='100%'>
-                <FormControl id='cvc'>
-                  <FormLabel
-                    fontWeight='semibold'
-                    placeholder='+1(217) 555-0113'
-                    fontSize='3xs'
-                    color='neutral.grayDark'
-                  >
-                    CVC
-                  </FormLabel>
-
-                  <Input
-                    type='password'
-                    background='neutral.white'
-                    _placeholder={{ color: 'neutral.gray' }}
-                    borderRadius='8px'
-                    fontSize='2xs'
-                    placeholder='XXX'
-                  />
-                </FormControl>
-              </GridItem>
-            </Grid>
-          </Box>
-          <Box pt={5}>
-            <Grid templateColumns='repeat(1, 1fr)' gap={4}>
-              <GridItem w='100%'>
-                <FormControl id='cardholder'>
-                  <FormLabel fontWeight='semibold' fontSize='3xs' color='neutral.grayDark'>
-                    Cardholder
-                  </FormLabel>
-
-                  <Input
-                    type='text'
-                    background='neutral.white'
-                    _placeholder={{ color: 'neutral.gray' }}
-                    borderRadius='8px'
-                    fontSize='2xs'
-                    placeholder='Enter name on card'
-                  />
-                </FormControl>
-              </GridItem>
-            </Grid>
-          </Box>
-
-          <Box pt={5} mb={3}>
-            <Grid templateColumns='repeat(1, 1fr)' gap={4}>
-              <GridItem w='100%'>
-                <FormControl>
-                  <FormLabel fontWeight='semibold' fontSize='3xs' color='neutral.grayDark'>
-                    Card type
-                  </FormLabel>
-                  <Select required fontSize='2xs' placeholder='Select country' id='cardtype' defaultValue={'visa'}>
-                    <option value={'visa'}>Visa</option>
-                    <option value={'mastercard'}>Master Card</option>
-                  </Select>
-                </FormControl>
-              </GridItem>
-            </Grid>
-          </Box>
-
-          <Box pt={5} display='flex' justifyContent='flex-end'>
-            <Button
-              type='submit'
-              w={{ base: '100%', md: 'initial' }}
-              background='neutral.white'
-              fontSize='2xs'
-              fontWeight='bold'
-              variant='solid'
-              color='primary.default'
-              borderWidth='1px'
-              borderColor='primary.default'
-              _hover={{
-                background: 'primary.default',
-                color: 'neutral.white',
-                borderWidth: '1px',
-                borderColor: 'primary.default'
-              }}
-              py={5}
-              me='20px'
-            >
-              Add new payment method
-            </Button>
-          </Box>
+          <NewPaymentMethod updateCreditCard={updateCreditCard} />
         </Box>
       </Box>
     </>
