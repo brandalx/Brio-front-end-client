c49bf2115de612815f5b13f4b01e8a9de375b79c
brndalx <113251192+brandalx@users.noreply.github.com>
2023-05-26 22:42:13 +0300

    feat: handler for canceled order status

--- src/user/userPages/Order.jsx
+++ src/user/userPages/Order.jsx
@@ -102,11 +102,42 @@ export default function Order() {
     }
   };
 
-  useEffect(() => {
-    const timer = setInterval(updateState, 60 * 100);
+  const findOrder2 = (orderId) => {
+    if (userArr && userArr.orders) {
+      const order = userArr.orders.find((item) => item.orderId === orderId);
+      if (order) {
+        return order;
+      } else {
+        return 'Status error';
+      }
+    } else {
+      return 'User data not available';
+    }
+  };
 
-    return () => clearInterval(timer);
-  }, [placed, prepared, delivery, delivered]);
+  useEffect(() => {
+    const currentOrder = findOrder2(params['id']);
+    if (currentOrder.status === 'Canceled') {
+      setPlaced(true);
+      setPrepared(true);
+      setDelivery(true);
+      setDelivered(true);
+    } else if (currentOrder.status === 'Placed') {
+      setPlaced(true);
+    } else if (currentOrder.status === 'Prepared') {
+      setPlaced(true);
+      setPrepared(true);
+    } else if (currentOrder.status === 'Delivery') {
+      setPlaced(true);
+      setPrepared(true);
+      setDelivery(true);
+    } else if (currentOrder.status === 'Delivered') {
+      setPlaced(true);
+      setPrepared(true);
+      setDelivery(true);
+      setDelivered(true);
+    }
+  }, [userArr]);
   function formatTime(date) {
     const options = { hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
     return new Date(date).toLocaleTimeString('en-US', options);
@@ -173,83 +204,106 @@ export default function Order() {
               </Flex>
               <Grid mt={5} templateColumns='0.2fr 1fr 0.2fr 1fr 0.2fr 1fr 0.2fr' gap={2}>
                 <GridItem w='fit-content'>
-                  <Box>
-                    <OrderStatus istrue={placed} number={1} />
+                  <Skeleton borderRadius='16px' isLoaded={!loading}>
+                    <Box>
+                      <OrderStatus istrue={placed} number={1} />
+                    </Box>
+                  </Skeleton>
+                </GridItem>
+
+                <GridItem w='100%'>
+                  <Box h='100%' display='flex' alignItems='center'>
+                    <Skeleton w='100%' borderRadius='16px' isLoaded={!loading}>
+                      <Divider borderWidth='2px' borderColor={placed ? '#1ABF70' : 'neutral.gray'} />
+                    </Skeleton>
                   </Box>
+                </GridItem>
+                <GridItem w='100%'>
+                  <Skeleton borderRadius='16px' isLoaded={!loading}>
+                    <Box>
+                      <OrderStatus istrue={prepared} number={2} />
+                    </Box>
+                  </Skeleton>
                 </GridItem>
                 <GridItem w='100%'>
                   <Box h='100%' display='flex' alignItems='center'>
-                    <Divider borderWidth='2px' borderColor={placed ? '#1ABF70' : 'neutral.gray'} />
+                    <Skeleton w='100%' borderRadius='16px' isLoaded={!loading}>
+                      <Divider borderWidth='2px' borderColor={delivery ? '#1ABF70' : 'neutral.gray'} />
+                    </Skeleton>{' '}
                   </Box>
                 </GridItem>
                 <GridItem w='100%'>
-                  <Box>
-                    <OrderStatus istrue={prepared} number={2} />
-                  </Box>
-                </GridItem>
-                <GridItem w='100%'>
-                  <Box h='100%' display='flex' alignItems='center'>
-                    <Divider borderWidth='2px' borderColor={delivery ? '#1ABF70' : 'neutral.gray'} />
-                  </Box>
-                </GridItem>
-                <GridItem w='100%'>
-                  <Box>
-                    <OrderStatus istrue={delivery} number={3} />
-                  </Box>
+                  <Skeleton borderRadius='16px' isLoaded={!loading}>
+                    <Box>
+                      <OrderStatus istrue={delivery} number={3} />
+                    </Box>
+                  </Skeleton>
                 </GridItem>
                 <GridItem w='100%'>
                   <Box h='100%' display='flex' alignItems='center'>
-                    <Divider borderWidth='2px' borderColor={delivered ? '#1ABF70' : 'neutral.gray'} />
+                    <Skeleton w='100%' borderRadius='16px' isLoaded={!loading}>
+                      <Divider borderWidth='2px' borderColor={delivered ? '#1ABF70' : 'neutral.gray'} />
+                    </Skeleton>
                   </Box>
                 </GridItem>
                 <GridItem w='100%'>
-                  <Box>
-                    <OrderStatus istrue={delivered} number={4} />
-                  </Box>
+                  <Skeleton borderRadius='16px' isLoaded={!loading}>
+                    <Box>
+                      <OrderStatus istrue={delivered} number={4} />
+                    </Box>
+                  </Skeleton>
                 </GridItem>
               </Grid>
 
-              <Flex justifyContent='space-between'>
+              <Flex justifyContent='space-between' my={4}>
                 <Box>
-                  <Box mt={4}>
-                    <Text fontSize='2xs' color='neutral.black' fontWeight='bold'>
-                      Order placed
-                    </Text>
-                    <Text color='neutral.black' fontSize='3xs'>
-                      {/* todo: checkif order already delivered */}
-                      {placed ? formatTime(Date.now()) : ''}
-                    </Text>
-                  </Box>
+                  <Skeleton h='20px' borderRadius='16px' isLoaded={!loading}>
+                    <Box mt={4}>
+                      <Text fontSize='2xs' color='neutral.black' fontWeight='bold'>
+                        Order placed
+                      </Text>
+                      <Text color='neutral.black' fontSize='3xs'>
+                        {/* todo: checkif order already delivered */}
+                        {placed ? formatTime(Date.now()) : ''}
+                      </Text>
+                    </Box>
+                  </Skeleton>
                 </Box>
                 <Box>
-                  <Box mt={4}>
-                    <Text fontSize='2xs' color='neutral.black' fontWeight='bold'>
-                      Order being prepared
-                    </Text>
-                    <Text color='neutral.black' fontSize='3xs'>
-                      {prepared ? formatTime(Date.now()) : ''}
-                    </Text>
-                  </Box>
+                  <Skeleton h='20px' borderRadius='16px' isLoaded={!loading}>
+                    <Box mt={4}>
+                      <Text fontSize='2xs' color='neutral.black' fontWeight='bold'>
+                        Order being prepared
+                      </Text>
+                      <Text color='neutral.black' fontSize='3xs'>
+                        {prepared ? formatTime(Date.now()) : ''}
+                      </Text>
+                    </Box>
+                  </Skeleton>
                 </Box>
                 <Box>
-                  <Box mt={4}>
-                    <Text fontSize='2xs' color='neutral.black' fontWeight='bold'>
-                      Out for delivery
-                    </Text>
-                    <Text color='neutral.black' fontSize='3xs'>
-                      {delivery ? formatTime(Date.now()) : ''}
-                    </Text>
-                  </Box>
+                  <Skeleton h='20px' borderRadius='16px' isLoaded={!loading}>
+                    <Box mt={4}>
+                      <Text fontSize='2xs' color='neutral.black' fontWeight='bold'>
+                        Out for delivery
+                      </Text>
+                      <Text color='neutral.black' fontSize='3xs'>
+                        {delivery ? formatTime(Date.now()) : ''}
+                      </Text>
+                    </Box>
+                  </Skeleton>
                 </Box>
                 <Box>
-                  <Box mt={4}>
-                    <Text fontSize='2xs' color='neutral.black' fontWeight='bold'>
-                      Delivered
-                    </Text>
-                    <Text color='neutral.black' fontSize='3xs'>
-                      {delivered ? formatTime(Date.now()) : ''}
-                    </Text>
-                  </Box>
+                  <Skeleton h='20px' borderRadius='16px' isLoaded={!loading}>
+                    <Box mt={4}>
+                      <Text fontSize='2xs' color='neutral.black' fontWeight='bold'>
+                        Delivered
+                      </Text>
+                      <Text color='neutral.black' fontSize='3xs'>
+                        {delivered ? formatTime(Date.now()) : ''}
+                      </Text>
+                    </Box>
+                  </Skeleton>
                 </Box>
               </Flex>
             </Box>
