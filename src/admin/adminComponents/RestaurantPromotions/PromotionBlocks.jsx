4d3acf5b94c97526a0f2a35cf7d36ab7541502f0
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-06-09 13:28:28 +0300

    feat: svg images added(Location updated) + promotion page crafted

--- src/admin/adminComponents/RestaurantPromotions/PromotionBlocks.jsx
+++ src/admin/adminComponents/RestaurantPromotions/PromotionBlocks.jsx
@@ -1,19 +1,74 @@
-import React, {useState} from 'react';
-
-import {Box, Button, Container, Text} from "@chakra-ui/react";
+import React, {useEffect, useState} from 'react';
+import {Box, Button, Container, Image, Text, Grid, Divider} from "@chakra-ui/react";
+import {API_URL, handleApiGet} from "../../../services/apiServices";
+import ValidThrough from "../../../assets/svg/ValidThrough";
+import Location from "../../../assets/svg/Location";
 
 export default function PromotionBlocks() {
-    const [active, setActive] = useState(true);
+    const [promotions, setPromotion] = useState([]);
+    const fetchPromotions = async () => {
+        try {
+            const response = await handleApiGet(API_URL + '/admin/promotions');
+            console.log(response);
+            setPromotion(response);
+        } catch (error) {
+            console.error('Error fetching products:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPromotions();
+    }, []);
 
     return (
         <Box>
             <Container maxW='1110px'>
-                <Box display='flex' justifyContent='space-between' fontSize='sm' color='neutral.black'
-                     fontWeight='semibold'>
-
-                </Box>
+                <Grid templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)"]} gap={6}>
+                    {promotions.map((promotion, index) => {
+                        return (
+                            <Box key={index} display='flex' justifyContent='space-between' flexDirection='column'
+                                 borderRadius='16px' border='1px' borderColor='neutral.grayLight' p='16px'>
+                                <Box display='flex' justifyContent='space-between'>
+                                    <Box marginRight='10px'>
+                                        <Box textAlign='center' border='1px' borderRadius='full' width='53px'
+                                             height='24px' borderColor='success.default'>
+                                            <Text color='success.default' fontWeight='semibold' p='3px' fontSize='3xs'>Active</Text>
+                                        </Box>
+                                        <Text fontWeight='bold' fontSize='2xs'>
+                                            {promotion.discountDetails}
+                                        </Text>
+                                    </Box>
+                                    <Image
+                                        width='112px'
+                                        height='92px'
+                                        borderRadius='16px'
+                                        src={promotion.image}
+                                        objectFit='cover'
+                                        objectPosition='center'
+                                    />
+                                </Box>
+                                <Divider mt='10px' mb='8px'/>
+                                <Box display='flex' flexDirection='column'>
+                                    <Box>
+                                        <Box display='flex' flexDirection='row' alignItems='center' mt='4px'>
+                                          <ValidThrough/>
+                                            <Text marginLeft='6px' color='neutral.gray' fontSize='3xs' fontWeight='semibold'>Valid through</Text>
+                                        </Box>
+                                    <Text color='neutral.black' fontSize='2.5xs'>{promotion.availableDate}</Text>
+                                    </Box>
+                                    <Box>
+                                        <Box display='flex' flexDirection='row' alignItems='center' mt='4px'>
+                                            <Location w={12} h={12}/>
+                                            <Text marginLeft='6px' color='neutral.gray' fontSize='3xs' fontWeight='semibold'>Restaurant</Text>
+                                        </Box>
+                                     <Text color='neutral.black'>{promotion.restaurantName}</Text>
+                                    </Box>
+                                </Box>
+                            </Box>
+                        )
+                    })}
+                </Grid>
             </Container>
         </Box>
     );
-
 }
--- src/admin/adminComponents/RestaurantPromotions/PromotionsHeader.jsx
+++ src/admin/adminComponents/RestaurantPromotions/PromotionsHeader.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 
 import {Box, Button, Container, Text} from "@chakra-ui/react";
+import ModalCreatePromotion from "./ModalCreatePromotion";
 
-export default function PromotionsHeader() {
-    const [active, setActive] = useState(true);
+export default function PromotionsHeader({modal}) {
+    const [active, setActive] = useState('active');
 
     return (
         <Box>
@@ -27,14 +28,16 @@ export default function PromotionsHeader() {
                         }}
                         width='150px'
                         height='44px'
+                        onClick={()=>{}}
                     >
                         Create promotion
                     </Button>
+                    <ModalCreatePromotion isOpen={true}/>
                 </Box>
                 <Box backgroundColor='neutral.grayLightest' borderRadius='6px' mb='16px' mt='16px' pt='7px' pb='7px' display='flex' justifyContent='space-around'>
-                    <Button fontWeight='bold' fontSize='2xs' ml='4px' p='13px' w='100%' h='100%' color={active === 'active' ? 'white' : 'neutral.grayDark'} backgroundColor={active === 'active' ? 'neutral.grayDark' : 'primary.grayLightest'} onClick={() => setActive('active')}>Active</Button>
-                    <Button fontWeight='bold' fontSize='2xs' p='13px' w='100%' h='100%' color={active === 'scheduled' ? 'white' : 'neutral.grayDark'} backgroundColor={active === 'scheduled' ? 'neutral.grayDark' : 'primary.grayLightest'} onClick={() => setActive('scheduled')}>Scheduled</Button>
-                    <Button fontWeight='bold' fontSize='2xs' mr='4px' p='13px' w='100%' h='100%' color={active === 'expired' ? 'white' : 'neutral.grayDark'} backgroundColor={active === 'expired' ? 'neutral.grayDark' : 'primary.grayLightest'} onClick={() => setActive('expired')}>Expired</Button>
+                    <Button fontWeight='bold' fontSize='2xs' ml='4px' p='13px' w='100%' h='100%' color={active === 'active' ? 'white' : 'neutral.grayDark'} backgroundColor={active === 'active' ? 'neutral.black' : 'primary.grayLightest'} onClick={() => setActive('active')}>Active</Button>
+                    <Button fontWeight='bold' fontSize='2xs' p='13px' w='100%' h='100%' color={active === 'scheduled' ? 'white' : 'neutral.grayDark'} backgroundColor={active === 'scheduled' ? 'neutral.black' : 'primary.grayLightest'} onClick={() => setActive('scheduled')}>Scheduled</Button>
+                    <Button fontWeight='bold' fontSize='2xs' mr='4px' p='13px' w='100%' h='100%' color={active === 'expired' ? 'white' : 'neutral.grayDark'} backgroundColor={active === 'expired' ? 'neutral.black' : 'primary.grayLightest'} onClick={() => setActive('expired')}>Expired</Button>
                 </Box>
             </Container>
         </Box>
