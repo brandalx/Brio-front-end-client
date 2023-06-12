55b3e982654cbe093b27618bd41863ca1c80953e
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-06-12 22:24:36 +0300

    feat: CustomerProfile.jsx + RecentOrders.jsx blocks added

---
+++ src/admin/adminComponents/RestaurantCustomers/CustomerProfile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box, Container, Text } from '@chakra-ui/react';
+import Vector from '../../../assets/svg/Vector';
+import { Link } from 'react-router-dom';
+
+export default function CustomerProfile() {
+  return (
+    <Box>
+      <Container maxW='1132px' pb='50px' paddingLeft={0}>
+        <Text color='neutral.black' fontWeight='semibold' fontSize='sm'>
+          Customer profile
+        </Text>
+          <Box>
+
+          </Box>
+      </Container>
+    </Box>
+  );
+}
--- src/admin/adminComponents/RestaurantCustomers/UserDetails.jsx
+++ src/admin/adminComponents/RestaurantCustomers/UserDetails.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { Box, Container, Text } from '@chakra-ui/react';
+import { Box, Container, Grid, Text } from '@chakra-ui/react';
 import Vector from '../../../assets/svg/Vector';
 import { Link } from 'react-router-dom';
+import CustomerProfile from './CustomerProfile';
+import RecentOrders from './RecentOrders';
 
 export default function UserDetails() {
   return (
     <Box>
       <Container maxW='1132px' pb='50px'>
-        <Link
-          to='/admin/restaurant/customers'
-          style={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}
-        >
-          <Vector w='8px' h='12px' />
-          <Text marginLeft='12px'>Restaurant customers</Text>
-        </Link>
+        <Box paddingBottom='30px' display='flex'>
+          <Link
+            to='/admin/restaurant/customers'
+            style={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}
+          >
+            <Vector w='8px' h='12px' />
+            <Text marginLeft='12px' color='neutral.grayDark' fontWeight='bold' fontSize='2.5xs'>
+              Restaurant customers
+            </Text>
+          </Link>
+        </Box>
+        <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(12, 1fr)' }} gap={6}>
+          <Box gridColumn={{ base: 'span 1', md: 'span 4' }}>
+            <CustomerProfile />
+          </Box>
+          <Box gridColumn={{ base: 'span 1', md: 'span 8' }}>
+            <RecentOrders />
+          </Box>
+        </Grid>
       </Container>
     </Box>
   );
