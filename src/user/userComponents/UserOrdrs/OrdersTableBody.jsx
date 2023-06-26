e2feb0cd2a4aadf447ad32822f79a996d61465af
brndalx <113251192+brandalx@users.noreply.github.com>
2023-06-26 14:53:20 +0300

    feat: ui improvements

--- src/user/userComponents/UserOrdrs/OrdersTableBody.jsx
+++ src/user/userComponents/UserOrdrs/OrdersTableBody.jsx
@@ -20,6 +20,7 @@ import ThreeDots from '../../../assets/svg/ThreeDots';
 import { API_URL, handleApiGet } from '../../../services/apiServices';
 import colorstatus from './colorsObject.json';
 import { Link } from 'react-router-dom';
+import { Button } from '@chakra-ui/react';
 
 export default function OrdersTableBody() {
   const [isTablet] = useMediaQuery('(max-width: 1199px)');
@@ -90,139 +91,141 @@ export default function OrdersTableBody() {
     return order ? order.userdata.status : undefined;
   };
   return (
-    <Tbody>
-      {!loading &&
-        arr.map((item) => {
-          const formattedDate = formatDate(item.creationDate);
-          return (
-            <Tr key={item._id} transition='all 0.2s' _hover={{ bg: 'bg', transition: 'all 0.2s', cursor: 'pointer' }}>
-              <Td pl={isMobile ? '10px' : ''} pt='19.5px' pb='19.5px' fontSize='2xs' color='neutral.grayDark'>
-                {item._id.substring(item._id.length - 6, item._id.length)}
-              </Td>
-              <Td
-                justifyContent='start'
-                flexDirection='row-reverse'
-                display='flex'
-                alignItems='center'
-                pt='19.5px'
-                pb='19.5px'
-                fontSize='2xs'
-                color='neutral.grayDark'
-                fontWeight='semibold'
-              >
-                {item.restaurant.length > 0 &&
-                  item.restaurant.map((item2, index2) => {
-                    return (
-                      <Box key={index2} bg='neutral.grayLightest' borderRadius='100px' px={4} py={1} me={2}>
-                        {getRestaurantName(item2)}
-                      </Box>
-                    );
-                  })}
-              </Td>
-              <Td pt='19.5px' pb='19.5px' fontSize='2xs' color='neutral.grayDark'>
-                {formattedDate}
-              </Td>
-              <Td pt='19.5px' pb='19.5px' fontSize='2xs' color='neutral.grayDark'>
-                {formatTime(item.creationDate)}
-              </Td>
-              <Td
-                pr={isMobile ? '0' : ''}
-                pl={isMobile ? '5px' : ''}
-                pt='10px'
-                pb='10px'
-                fontSize='2.5xs'
-                color='neutral.black'
-                fontWeight='semibold'
-              >
-                <Flex alignItems='center'>
-                  <Box as='span' me={2}>
-                    <Status color={(orders.length > 0 && colorstatus[findOrder(item.orderRef)]) || 'red'} />
-                  </Box>
-                  {(orders.length > 0 && findOrder(item.orderRef)) || 'Undefined'}
-                </Flex>
-              </Td>
+    <>
+      <Tbody>
+        {!loading &&
+          arr.map((item) => {
+            const formattedDate = formatDate(item.creationDate);
+            return (
+              <Tr key={item._id} transition='all 0.2s' _hover={{ bg: 'bg', transition: 'all 0.2s', cursor: 'pointer' }}>
+                <Td pl={isMobile ? '10px' : ''} pt='19.5px' pb='19.5px' fontSize='2xs' color='neutral.grayDark'>
+                  {item._id.substring(item._id.length - 6, item._id.length)}
+                </Td>
+                <Td
+                  justifyContent='start'
+                  flexDirection='row-reverse'
+                  display='flex'
+                  alignItems='center'
+                  pt='19.5px'
+                  pb='19.5px'
+                  fontSize='2xs'
+                  color='neutral.grayDark'
+                  fontWeight='semibold'
+                >
+                  {item.restaurant.length > 0 &&
+                    item.restaurant.map((item2, index2) => {
+                      return (
+                        <Box key={index2} bg='neutral.grayLightest' borderRadius='100px' px={4} py={1} me={2}>
+                          {getRestaurantName(item2)}
+                        </Box>
+                      );
+                    })}
+                </Td>
+                <Td pt='19.5px' pb='19.5px' fontSize='2xs' color='neutral.grayDark'>
+                  {formattedDate}
+                </Td>
+                <Td pt='19.5px' pb='19.5px' fontSize='2xs' color='neutral.grayDark'>
+                  {formatTime(item.creationDate)}
+                </Td>
+                <Td
+                  pr={isMobile ? '0' : ''}
+                  pl={isMobile ? '5px' : ''}
+                  pt='10px'
+                  pb='10px'
+                  fontSize='2.5xs'
+                  color='neutral.black'
+                  fontWeight='semibold'
+                >
+                  <Flex alignItems='center'>
+                    <Box as='span' me={2}>
+                      <Status color={(orders.length > 0 && colorstatus[findOrder(item.orderRef)]) || 'red'} />
+                    </Box>
+                    {(orders.length > 0 && findOrder(item.orderRef)) || 'Undefined'}
+                  </Flex>
+                </Td>
 
-              <Td
-                transition='all 0.3s'
-                _hover={{ color: 'primary.default', transition: 'all 0.3s' }}
-                pl={isMobile ? '0' : ''}
-                pr={isMobile ? '0' : ''}
-                textAlign='center'
-                pt='10px'
-                pb='10px'
-                fontSize='2xs'
-                fontWeight='bold'
-                color='neutral.black'
-              >
-                ${item.paymentSummary.totalAmount}
-              </Td>
-              <Td
-                position={isMobile ? 'relative' : ''}
-                right={isMobile ? '10px' : '0'}
-                pl={0}
-                pr={0}
-                pt='10px'
-                pb='10px'
-                fontSize='2xs'
-                fontWeight='bold'
-                color='neutral.black'
-              >
-                <Menu>
-                  <MenuButton
-                    _hover={{
-                      color: 'neutral.black',
-                      borderColor: 'neutral.lightest'
-                    }}
-                    fontSize='2xs'
-                    color='neutral.gray'
-                    fontWeight='bold'
-                  >
-                    <ThreeDots />
-                  </MenuButton>
+                <Td
+                  transition='all 0.3s'
+                  _hover={{ color: 'primary.default', transition: 'all 0.3s' }}
+                  pl={isMobile ? '0' : ''}
+                  pr={isMobile ? '0' : ''}
+                  textAlign='center'
+                  pt='10px'
+                  pb='10px'
+                  fontSize='2xs'
+                  fontWeight='bold'
+                  color='neutral.black'
+                >
+                  ${item.paymentSummary.totalAmount}
+                </Td>
+                <Td
+                  position={isMobile ? 'relative' : ''}
+                  right={isMobile ? '10px' : '0'}
+                  pl={0}
+                  pr={0}
+                  pt='10px'
+                  pb='10px'
+                  fontSize='2xs'
+                  fontWeight='bold'
+                  color='neutral.black'
+                >
+                  <Menu>
+                    <MenuButton
+                      _hover={{
+                        color: 'neutral.black',
+                        borderColor: 'neutral.lightest'
+                      }}
+                      fontSize='2xs'
+                      color='neutral.gray'
+                      fontWeight='bold'
+                    >
+                      <ThreeDots />
+                    </MenuButton>
 
-                  <MenuList>
-                    {' '}
-                    <Link to={'/user/order/' + item.orderRef}>
-                      <MenuItem fontWeight='medium'>More info</MenuItem>{' '}
-                    </Link>
-                  </MenuList>
-                </Menu>
-              </Td>
-            </Tr>
-          );
-        })}
-      {/*  skeleton */}
-      {loading &&
-        skeletonarr.map((item, index) => {
-          return (
-            <Tr key={index}>
-              <Td>
-                {' '}
-                <Skeleton borderRadius='16px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
-              </Td>
-              <Td>
-                {' '}
-                <Skeleton borderRadius='12px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
-              </Td>
-              <Td>
-                {' '}
-                <Skeleton borderRadius='16px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
-              </Td>
-              <Td>
-                {' '}
-                <Skeleton borderRadius='16px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
-              </Td>
-              <Td>
-                {' '}
-                <Skeleton borderRadius='16px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
-              </Td>
-              <Td>
-                {' '}
-                <Skeleton borderRadius='16px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
-              </Td>
-            </Tr>
-          );
-        })}
-    </Tbody>
+                    <MenuList>
+                      {' '}
+                      <Link to={'/user/order/' + item.orderRef}>
+                        <MenuItem fontWeight='medium'>More info</MenuItem>{' '}
+                      </Link>
+                    </MenuList>
+                  </Menu>
+                </Td>
+              </Tr>
+            );
+          })}
+        {/*  skeleton */}
+        {loading &&
+          skeletonarr.map((item, index) => {
+            return (
+              <Tr key={index}>
+                <Td>
+                  {' '}
+                  <Skeleton borderRadius='16px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
+                </Td>
+                <Td>
+                  {' '}
+                  <Skeleton borderRadius='12px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
+                </Td>
+                <Td>
+                  {' '}
+                  <Skeleton borderRadius='16px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
+                </Td>
+                <Td>
+                  {' '}
+                  <Skeleton borderRadius='16px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
+                </Td>
+                <Td>
+                  {' '}
+                  <Skeleton borderRadius='16px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
+                </Td>
+                <Td>
+                  {' '}
+                  <Skeleton borderRadius='16px' isLoaded={!loading} minH='20px' w='100%'></Skeleton>
+                </Td>
+              </Tr>
+            );
+          })}
+      </Tbody>
+    </>
   );
 }
