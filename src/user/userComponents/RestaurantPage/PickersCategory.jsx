76f6b56ef7e3981e25c05771475fd4110952566c
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-19 03:00:13 +0300

    feat: pick category

--- src/user/userComponents/RestaurantPage/PickersCategory.jsx
+++ src/user/userComponents/RestaurantPage/PickersCategory.jsx
@@ -1,5 +1,6 @@
-import { Menu, Box, Text } from '@chakra-ui/react';
-import React from 'react';
+import { Menu, Box, Text, GridItem, Grid } from '@chakra-ui/react';
+
+import React, { useCallback, useEffect, useState } from 'react';
 
 export default function PickersCategory({
   categories,
@@ -9,13 +10,66 @@ export default function PickersCategory({
   picked,
   setIsPicked
 }) {
+  const [isPressed, setIsPressed] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleClick = useCallback(
+    (categoryIndex) => {
+      setIsPressed(!isPressed);
+      // Update the picked state for the clicked category
+      const updatedCategories = categories.map((category, index) => {
+        if (index === categoryIndex) {
+          return {
+            ...category,
+            picked: !category.picked
+          };
+        }
+        return category;
+      });
+      SetCategories(updatedCategories);
+    },
+    [isPressed, categories, SetCategories]
+  );
+
   return (
     <div>
       <Box>
-        {categories.length > 0 &&
-          categories.map((item, index) => {
-            return <Text key={index}>{item.categoryName}</Text>;
-          })}
+        <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: '1fr 1fr 1fr ' }} gap={2}>
+          {categories.length > 0 &&
+            categories.map((item, index) => {
+              const bgColor = item.picked ? 'primary.light' : isHovered ? 'primary.lightest' : 'neutral.white';
+              const borderColor = item.picked
+                ? 'primary.default'
+                : isHovered
+                ? 'neutral.white'
+                : 'neutral.grayLightest';
+              const borderWidth = '2px';
+              const textColor = item.picked ? 'primary.default' : 'primary.gray';
+
+              return (
+                <GridItem
+                  key={index}
+                  fontWeight='bold'
+                  cursor='pointer'
+                  w='100%'
+                  h='auto'
+                  borderRadius={16}
+                  bg={bgColor}
+                  borderColor={borderColor}
+                  borderWidth={borderWidth}
+                  p={2}
+                  background='neutral.white'
+                  transition='all 0.3s'
+                  _hover={{ cursor: 'pointer', background: 'primary.lightest', transition: 'all 0.3s' }}
+                  onClick={() => handleClick(index)} // Pass the category index to handleClick
+                >
+                  <Box textAlign='center'>
+                    <Text color={textColor}>{item.categoryName}</Text>
+                  </Box>
+                </GridItem>
+              );
+            })}
+        </Grid>
       </Box>
     </div>
   );
