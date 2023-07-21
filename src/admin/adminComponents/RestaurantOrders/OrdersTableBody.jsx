a68d68fc631065148dd4fcbbc8994b52a990680d
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-07-21 23:09:07 +0300

    feat: table fixed

--- src/admin/adminComponents/RestaurantOrders/OrdersTableBody.jsx
+++ src/admin/adminComponents/RestaurantOrders/OrdersTableBody.jsx
@@ -189,7 +189,7 @@ export default function OrdersTableBody() {
               const relatedProduct = products.find(
                 (p) => p._id === product.productId && product.restaurantId === restaurantId
               );
-              return total + (relatedProduct ? relatedProduct.price * product.amount : 0);
+              return total + (relatedProduct ? product.priceItem : 0);
             }, 0);
 
             // Adding the shipping and tips to the total cost of products
