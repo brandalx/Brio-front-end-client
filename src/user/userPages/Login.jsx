7a05e80e809d990b51d0699eb69721077770607b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-26 18:36:48 +0300

    feat: show/hide password  + to lower case email

--- src/user/userPages/Login.jsx
+++ src/user/userPages/Login.jsx
@@ -34,6 +34,8 @@ export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+  const handleClickShow = () => setShow(!show);
   const {
     handleSubmit,
     register,
@@ -50,6 +52,8 @@ export default function Login() {
     try {
       // const url = API_URL + "/videos/"+params["id"];
       const url = API_URL + '/users/login';
+      _bodyData.email = _bodyData.email.toLowerCase();
+      console.log(_bodyData);
       const data = await handleApiMethod(url, 'POST', _bodyData);
       if (data.token) {
         toast({
@@ -141,22 +145,70 @@ export default function Login() {
                         <FormLabel color='neutral.grayDark' fontWeight='semibold' fontSize='3xs'>
                           Password
                         </FormLabel>
+                        <InputGroup>
+                          <Input
+                            type={show ? 'text' : 'password'}
+                            {...register('password', {
+                              required: { value: true, message: 'This field is required' },
+                              minLength: { value: 6, message: 'Minimum length should be 6' }
+                            })}
+                            id='password'
+                            background='neutral.white'
+                            _placeholder={{ color: 'neutral.gray' }}
+                            borderRadius='8px'
+                            fontSize='2xs'
+                            placeholder='min. 8 characters'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                          {password.length > 0 && (
+                            <InputRightElement me={2}>
+                              <Button h='1.75rem' size='2xs' onClick={handleClickShow}>
+                                {show ? (
+                                  <span>
+                                    <svg
+                                      xmlns='http://www.w3.org/2000/svg'
+                                      className='icon icon-tabler icon-tabler-eye'
+                                      width={18}
+                                      height={18}
+                                      viewBox='0 0 24 24'
+                                      strokeWidth={2}
+                                      stroke='#4E60FF'
+                                      fill='none'
+                                      strokeLinecap='round'
+                                      strokeLinejoin='round'
+                                    >
+                                      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                                      <path d='M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0' />
+                                      <path d='M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6' />
+                                    </svg>
+                                  </span>
+                                ) : (
+                                  <span>
+                                    <svg
+                                      xmlns='http://www.w3.org/2000/svg'
+                                      className='icon icon-tabler icon-tabler-eye-off'
+                                      width={18}
+                                      height={18}
+                                      viewBox='0 0 24 24'
+                                      strokeWidth={2}
+                                      stroke='#C7C8D2'
+                                      fill='none'
+                                      strokeLinecap='round'
+                                      strokeLinejoin='round'
+                                    >
+                                      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                                      <path d='M10.585 10.587a2 2 0 0 0 2.829 2.828' />
+                                      <path d='M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87' />
+                                      <path d='M3 3l18 18' />
+                                    </svg>
+                                  </span>
+                                )}
+                              </Button>
+                            </InputRightElement>
+                          )}
+                        </InputGroup>
 
-                        <Input
-                          {...register('password', {
-                            required: { value: true, message: 'This field is required' },
-                            minLength: { value: 6, message: 'Minimum length should be 6' }
-                          })}
-                          id='password'
-                          type='password'
-                          background='neutral.white'
-                          _placeholder={{ color: 'neutral.gray' }}
-                          borderRadius='8px'
-                          fontSize='2xs'
-                          placeholder='min. 8 characters'
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        />
                         <FormErrorMessage p={0} mt={2} fontSize='3xs'>
                           {errors.password && errors.password.message}
                         </FormErrorMessage>
