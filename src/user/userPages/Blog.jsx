0e8f6813390e9dc1d08d8371b483807be4d25868
brndalx <113251192+brandalx@users.noreply.github.com>
2023-07-29 04:17:35 +0300

    feat: blog fetching general blog data

--- src/user/userPages/Blog.jsx
+++ src/user/userPages/Blog.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { API_URL, handleApiGet } from '../../services/apiServices';
+import { useParams } from 'react-router-dom';
+import { Box } from '@chakra-ui/react';
 
 export default function Blog() {
-  return <div>Blog</div>;
+  const [arr, setArr] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  const params = useParams();
+  const handleBlogApi = async () => {
+    const url = API_URL + '/blogs/blog/single/' + params['id'];
+    try {
+      const data = await handleApiGet(url);
+
+      setArr(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleBlogApi();
+  }, []);
+  return (
+    <Box>
+      <Box>blog info</Box>
+    </Box>
+  );
 }
