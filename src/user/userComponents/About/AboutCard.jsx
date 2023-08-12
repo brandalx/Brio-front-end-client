a365a76de46b55f521770645a8226c1221c8901b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-08-12 07:39:27 +0300

    feat: about us cards, json and layout

---
+++ src/user/userComponents/About/AboutCard.jsx
@@ -0,0 +1,91 @@
+import { Box, Grid, GridItem, Image, Link, Text } from '@chakra-ui/react';
+import React from 'react';
+
+export default function AboutCard({ data, index }) {
+  return (
+    <Box data-aos='fade-up' my={4}>
+      <Box
+        border='1px'
+        borderColor='neutral.grayLightest'
+        py={5}
+        overflow='hidden'
+        minH={{ base: '350px', md: '250px' }}
+        bg={() => (localStorage.getItem('colormode') === 'dark' ? '#363654' : '#FCFCFC')}
+        borderRadius='lg'
+      >
+        <Box w='100%'>
+          <Box px={8} display='flex' flexDir={index % 2 === 0 ? 'row' : 'row-reverse'} alignItems='center'>
+            <Box>
+              <Image h='150px' rounded='lg' src={data.image} />
+            </Box>
+
+            <Box ps={3}>
+              <Box pt='5'>
+                <Text color='neutral.black' fontSize='lg' fontWeight='extrabold'>
+                  {data.name}
+                </Text>
+                <Box
+                  textAlign={index % 2 === 0 ? 'start' : 'end'}
+                  fontWeight='normal'
+                  color='neutral.grayLight'
+                  fontSize='3xs'
+                >
+                  {data.age}
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+
+          <Box width={{ base: '100%', md: '90%' }}>
+            <Box px={5}>
+              <Box my={2}>
+                <Text fontWeight='bold' color='neutral.black' fontSize='xs'>
+                  Bio:
+                </Text>{' '}
+                <Text mt={1} color='neutral.gray' fontSize='2xs'>
+                  {data.bio}
+                </Text>
+              </Box>
+
+              <Box my={4}>
+                <Text fontWeight='bold' color='neutral.black' fontSize='xs'>
+                  Responsible in Brio:
+                </Text>{' '}
+                <Text mt={1} color='neutral.gray' fontSize='2xs'>
+                  {data.role}
+                </Text>
+              </Box>
+
+              <Box my={4}>
+                <Text fontWeight='bold' color='neutral.black' fontSize='xs'>
+                  Contact information:
+                </Text>{' '}
+                {data.contacts.map((contact, idx) => (
+                  <Box display='flex' key={idx}>
+                    <Text mt={1} fontWeight='medium' color='neutral.black' fontSize='2xs'>
+                      {contact.type}:
+                    </Text>
+                    <Link
+                      _hover={{ color: 'primary.default' }}
+                      ms={2}
+                      textDecor='underline'
+                      rel='noreferrer'
+                      target='_blank'
+                      href={contact.link}
+                      mt={1}
+                      fontWeight='medium'
+                      color='neutral.black'
+                      fontSize='2xs'
+                    >
+                      {contact.displayText}
+                    </Link>
+                  </Box>
+                ))}
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
--- src/user/userPages/About.jsx
+++ src/user/userPages/About.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Button, Container, Text, Flex, Icon } from '@chakra-ui/react';
+import { Box, Button, Container, Text, Flex, Grid, Icon, Image, GridItem } from '@chakra-ui/react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FaChevronLeft } from 'react-icons/fa';
 import Spline from '@splinetool/react-spline';
 import Preloader from '../../components/Loaders/preloader';
+import AboutCard from '../userComponents/About/AboutCard';
+import dataJson from '../userComponents/About/about.json';
+import Revenue from '../../assets/svg/Revenue';
+import { Heading } from '@chakra-ui/react';
 
 export default function About() {
   const navigate = useNavigate();
@@ -17,11 +21,24 @@ export default function About() {
     setheightchange(250);
     setLoading(false);
   };
+  const [numbers, setNumbers] = useState([]);
 
+  const generateNumbers = () => {
+    const firstNumber = Math.round(Math.random()); // This will give either 0 or 1.
+    let secondNumber;
+    do {
+      secondNumber = Math.round(Math.random());
+    } while (secondNumber === firstNumber); // Keep generating until different from firstNumber
+
+    setNumbers([firstNumber, secondNumber]);
+  };
+  useEffect(() => {
+    generateNumbers();
+  }, []);
   return (
     <Box py='60px' bg='neutral.white'>
       <Preloader colorss={localStorage.getItem('colormode') === 'dark' ? '#2B2B43' : 'white'} loading={loading} />
-      <Container maxW='1110px' pt={15}>
+      <Container maxW='960px' pt={15}>
         <Box my={4}>
           <Button _hover={{ transform: 'scale(1.010)' }} transition='transform 0.2s ease-in-out'>
             <Flex alignItems='center'>
@@ -42,7 +59,7 @@ export default function About() {
               color={() => (localStorage.getItem('colormode') === 'dark' ? 'black' : 'white')}
               fontWeight='black'
             >
-              About Us
+              About Brio
             </Text>
 
             <Text
@@ -63,13 +80,255 @@ export default function About() {
         </Box>
 
         <Box pt='20px'>
-          <Text fontSize={{ base: 'sm', md: 'dm' }} color='neutral.black'>
-            We started this company with a passion for creating the best online experiences. Our team is dedicated to
-            ensuring that our products and services exceed your expectations. With a foundation built on trust,
-            innovation, and customer satisfaction, we aim to become the leaders in our industry. Thank you for being a
-            part of our journey.
+          <Text textAlign='center' fontSize='xl' color='primary.default' fontWeight='extrabold'>
+            Meet Brio Developers!
           </Text>
         </Box>
+        <Box my={5}>
+          {numbers.map((randomIndex, index) => {
+            const item = dataJson[randomIndex];
+            return (
+              <Box key={randomIndex}>
+                <AboutCard index={index} data={item} />
+              </Box>
+            );
+          })}
+        </Box>
+        <Box data-aos='fade-up'>
+          <Box my={4}>
+            <Box
+              border='1px'
+              borderColor='neutral.grayLightest'
+              py={5}
+              overflow='hidden'
+              minH={{ base: '350px', md: '250px' }}
+              bg={() => (localStorage.getItem('colormode') === 'dark' ? '#363654' : '#FCFCFC')}
+              borderRadius='lg'
+            >
+              <Box px={8}>
+                <Image src='http://cdn.mcauto-images-production.sendgrid.net/27548861a3bba7f7/960bec92-ae6e-4946-99af-34de5bcd508b/4393x3391.png' />
+                <Box>
+                  <Text color='neutral.black' fontSize='60px' textAlign='center' fontWeight='extrabold'>
+                    Brio Sources
+                  </Text>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+
+        <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: '1fr 2fr 1fr' }} gap={6}>
+          <GridItem w='100%' ata-aos='fade-up'>
+            <Box my={4}>
+              <Box>
+                <Box
+                  py={5}
+                  minH={{ base: '350px', md: '250px' }}
+                  bg={() => (localStorage.getItem('colormode') === 'dark' ? '#363654' : 'white')}
+                  display='flex'
+                  justifyContent='center'
+                  alignItems='center'
+                  _hover={{
+                    cursor: 'pointer',
+                    transition: 'all 0.3s',
+                    bg: 'primary.light',
+                    borderColor: 'primary.light'
+                  }}
+                  _active={{
+                    cursor: 'pointer',
+                    transition: 'all 0.3s',
+                    bg: 'primary.light',
+                    borderColor: 'primary.default'
+                  }}
+                  borderRadius='16px'
+                  transition='all 0.3s'
+                  borderWidth='1px'
+                >
+                  <Flex alignItems='center'>
+                    <Box p={4}>
+                      <Box p={5} borderRadius={12}>
+                        <Box
+                          mx='auto'
+                          display='flex'
+                          alignItems='center'
+                          justifyContent='center'
+                          bg={() => (localStorage.getItem('colormode') === 'dark' ? '#363654' : 'primary.light')}
+                          color='black'
+                          px={'8px'}
+                          py={'8px'}
+                          borderRadius='16px'
+                        >
+                          <Box mx='auto' display='flex' alignItems='center' justifyContent='center' p='8px'>
+                            <Revenue />
+                          </Box>
+                        </Box>
+                      </Box>
+                      <Box textAlign='center' px={2}>
+                        <Heading
+                          color={() =>
+                            localStorage.getItem('colormode') === 'dark' ? 'primary.default' : 'neutral.grayDark'
+                          }
+                          fontSize='2xs'
+                          fontWeight='bold'
+                        >
+                          Front end repo
+                        </Heading>
+                        <Text
+                          fontSize='3xs'
+                          color={() =>
+                            localStorage.getItem('colormode') === 'dark' ? 'primary.default' : 'neutral.grayDark'
+                          }
+                        >
+                          link
+                        </Text>
+                      </Box>
+                    </Box>
+                  </Flex>
+                </Box>
+              </Box>
+            </Box>
+          </GridItem>
+          <GridItem w='100%' ata-aos='fade-up'>
+            <Box my={4}>
+              <Box>
+                <Box
+                  py={5}
+                  minH={{ base: '350px', md: '250px' }}
+                  bg={() => (localStorage.getItem('colormode') === 'dark' ? '#363654' : 'white')}
+                  display='flex'
+                  justifyContent='center'
+                  alignItems='center'
+                  _hover={{
+                    cursor: 'pointer',
+                    transition: 'all 0.3s',
+                    bg: 'primary.light',
+                    borderColor: 'primary.light'
+                  }}
+                  _active={{
+                    cursor: 'pointer',
+                    transition: 'all 0.3s',
+                    bg: 'primary.light',
+                    borderColor: 'primary.default'
+                  }}
+                  borderRadius='16px'
+                  transition='all 0.3s'
+                  borderWidth='1px'
+                >
+                  <Flex alignItems='center'>
+                    <Box p={4}>
+                      <Box p={5} borderRadius={12}>
+                        <Box
+                          mx='auto'
+                          display='flex'
+                          alignItems='center'
+                          justifyContent='center'
+                          bg={() => (localStorage.getItem('colormode') === 'dark' ? '#363654' : 'primary.light')}
+                          color='black'
+                          px={'8px'}
+                          py={'8px'}
+                          borderRadius='16px'
+                        >
+                          <Box mx='auto' display='flex' alignItems='center' justifyContent='center' p='8px'>
+                            <Revenue />
+                          </Box>
+                        </Box>
+                      </Box>
+                      <Box textAlign='center' px={2}>
+                        <Heading
+                          color={() =>
+                            localStorage.getItem('colormode') === 'dark' ? 'primary.default' : 'neutral.grayDark'
+                          }
+                          fontSize='2xs'
+                          fontWeight='bold'
+                        >
+                          Brio architecture link
+                        </Heading>
+                        <Text
+                          fontSize='3xs'
+                          color={() =>
+                            localStorage.getItem('colormode') === 'dark' ? 'primary.default' : 'neutral.grayDark'
+                          }
+                        >
+                          FigJam
+                        </Text>
+                      </Box>
+                    </Box>
+                  </Flex>
+                </Box>
+              </Box>
+            </Box>
+          </GridItem>
+          <GridItem w='100%' ata-aos='fade-up'>
+            <Box my={4}>
+              <Box>
+                <Box
+                  py={5}
+                  minH={{ base: '350px', md: '250px' }}
+                  bg={() => (localStorage.getItem('colormode') === 'dark' ? '#363654' : 'white')}
+                  display='flex'
+                  justifyContent='center'
+                  alignItems='center'
+                  _hover={{
+                    cursor: 'pointer',
+                    transition: 'all 0.3s',
+                    bg: 'primary.light',
+                    borderColor: 'primary.light'
+                  }}
+                  _active={{
+                    cursor: 'pointer',
+                    transition: 'all 0.3s',
+                    bg: 'primary.light',
+                    borderColor: 'primary.default'
+                  }}
+                  borderRadius='16px'
+                  transition='all 0.3s'
+                  borderWidth='1px'
+                >
+                  <Flex alignItems='center'>
+                    <Box p={4}>
+                      <Box p={5} borderRadius={12}>
+                        <Box
+                          mx='auto'
+                          display='flex'
+                          alignItems='center'
+                          justifyContent='center'
+                          bg={() => (localStorage.getItem('colormode') === 'dark' ? '#363654' : 'primary.light')}
+                          color='black'
+                          px={'8px'}
+                          py={'8px'}
+                          borderRadius='16px'
+                        >
+                          <Box mx='auto' display='flex' alignItems='center' justifyContent='center' p='8px'>
+                            <Revenue />
+                          </Box>
+                        </Box>
+                      </Box>
+                      <Box textAlign='center' px={2}>
+                        <Heading
+                          color={() =>
+                            localStorage.getItem('colormode') === 'dark' ? 'primary.default' : 'neutral.grayDark'
+                          }
+                          fontSize='2xs'
+                          fontWeight='bold'
+                        >
+                          Back-end repo
+                        </Heading>
+                        <Text
+                          fontSize='3xs'
+                          color={() =>
+                            localStorage.getItem('colormode') === 'dark' ? 'primary.default' : 'neutral.grayDark'
+                          }
+                        >
+                          link
+                        </Text>
+                      </Box>
+                    </Box>
+                  </Flex>
+                </Box>
+              </Box>
+            </Box>
+          </GridItem>
+        </Grid>
       </Container>
     </Box>
   );
